cd72d896696fd583c3f1c7d04f78cccef45ce0c6
lfarrell <lfarrell@email.unc.edu>
2015-02-18 14:54:44 -0500

    copies of works directive and template should work and sort by publication date.

--- blakearchive/static/directives/copies-for-work/directive.js
+++ blakearchive/static/directives/copies-for-work/directive.js
@@ -3,29 +3,24 @@
  */
 
 angular.module('blake').directive("copiesForWork", function () {
-    scope.$watch('copies', function (data) {
-        if (!data) { return; }
-
-        data.sort(function(a, b) { return a.source.comp_date["@value"] - b.source.comp_date["@value"]; });
-
-        var copies = [];
-        data.forEach(function(d) {
-            copies.push(d);
-        });
-
-        scope.copies = copies;
-    });
-
     return {
         restrict: 'E',
-        scope: true,
         templateUrl: "/blake/static/directives/copies-for-work/template.html",
         controller: "CopiesForWorkController"
     }
 });
 
 angular.module('blake').controller("CopiesForWorkController", function ($scope, BlakeDataService) {
-    BlakeDataService.getCopiesForWork().then(function(data) {
-        $scope.copies = data;
-    })
+    $scope.$watch(BlakeDataService.getSelectedCopy, function (copy) {
+        if (copy) {
+            $scope.copy = copy;
+            BlakeDataService.getCopiesForWork(copy.bad_id).then(function (data) {
+                data.sort(function (a, b) {
+                    return a.source.comp_date["@value"] - b.source.comp_date["@value"];
+                });
+
+                $scope.objects = data;
+            });
+        }
+    });
 });
