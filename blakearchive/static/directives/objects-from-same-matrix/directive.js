4033e0eaa45727f89421482f44ee531ecc1d5a22
lfarrell <lfarrell@email.unc.edu>
2016-01-15 12:09:32 -0500

    Select all should work correctly. Includes original img & imgs to compare it to.Checkbox added to each img to select for comparison.

--- blakearchive/static/directives/objects-from-same-matrix/directive.js
+++ blakearchive/static/directives/objects-from-same-matrix/directive.js
@@ -23,9 +23,35 @@ angular.module('blake').controller("ObjectsFromSameMatrixController", ['$scope',
         }
     });
 
+    //
+    $scope.compareText = "Select All Objects";
+    $scope.selectedAll = false;
+
+    // Select all objects for comparison
     $scope.selectAll = function () {
-        for (var i = $scope.objects.length; i--;) {
-            BlakeDataService.addComparisonObject($scope.objects[i]);
+        var obj_size = $scope.objects.length;
+
+        if(!$scope.selectedAll) {
+            $scope.compareText = "Clear All Objects";
+            $scope.selectedAll = true;
+
+            // Main viewer img
+            BlakeDataService.addComparisonObject($scope.item);
+
+            // Add imgs to compare main viewer img too.
+            for (var i = obj_size; i--;) {
+                $scope.objects[i].Selected = true;
+                BlakeDataService.addComparisonObject($scope.objects[i]);
+            }
+        } else {
+            $scope.compareText = "Select All Objects";
+            $scope.selectedAll = false;
+            BlakeDataService.removeComparisonObject($scope.item);
+
+            for (var j = obj_size; j--;) {
+                $scope.objects[j].Selected = false;
+                BlakeDataService.removeComparisonObject($scope.objects[j]);
+            }
         }
-    }
+    };
 }]);
