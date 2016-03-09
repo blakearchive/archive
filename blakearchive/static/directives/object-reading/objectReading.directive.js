43ae399091868bfd2e800a3e64aac356a0a6e1c2
Luke Aeschleman <lukeluke@email.unc.edu>
2016-03-09 11:06:57 -0500

    update reading view

--- blakearchive/static/directives/object-reading/objectReading.directive.js
+++ blakearchive/static/directives/object-reading/objectReading.directive.js
@@ -15,6 +15,15 @@
                 }
             }
         }
+        vm.getOvpSubtitle = function(object){
+            if(angular.isDefined(vm.copy)){
+                if(vm.copy.virtual == true){
+                    return 'Object '+object.object_number + object.full_object_id.replace(/object [\d]+/g,'');
+                } else {
+                    return object.full_object_id;
+                }
+            }
+        };
 
     }
 
