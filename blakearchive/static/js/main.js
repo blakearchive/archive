87cef33d08b7db19ef5fdae67cea5819a741526f
Luke Aeschleman <lukeluke@email.unc.edu>
2016-03-24 17:08:59 -0400

    compare selection routing
    -objectCompare & copy controller
    Selecting an object in the compare view will now update the url (copy
    and object ID), the copy tabs and the info tray

--- blakearchive/static/js/main.js
+++ blakearchive/static/js/main.js
@@ -799,4 +799,18 @@ angular.module('blake', ['ngRoute', 'ngSanitize', 'ui-rangeSlider', 'ui.bootstra
 
         $routeProvider.otherwise({redirectTo: directoryPrefix + '/'});
         $locationProvider.html5Mode(true);
+    }])
+
+    .run(['$route', '$rootScope', '$location', function ($route, $rootScope, $location) {
+        var original = $location.path;
+        $location.path = function (path, reload) {
+            if (reload === false) {
+                var lastRoute = $route.current;
+                var un = $rootScope.$on('$locationChangeSuccess', function () {
+                    $route.current = lastRoute;
+                    un();
+                });
+            }
+            return original.apply($location, [path]);
+        };
     }]);
