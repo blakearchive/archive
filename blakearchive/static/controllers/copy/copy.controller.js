13935c775c63508646492ce51533787799f200b0
Michael Fox <foxmichaelroy@gmail.com>
2016-04-04 21:54:42 -0400

    scroll to top upon object change

--- blakearchive/static/controllers/copy/copy.controller.js
+++ blakearchive/static/controllers/copy/copy.controller.js
@@ -73,6 +73,9 @@
 
         vm.changeObject = function(object){
             $location.search('objectId',object.object_id);
+            $('html, body').animate({
+                scrollTop: 0
+            }, 'slow');
         };
 
         vm.setObject = function(object_id){
