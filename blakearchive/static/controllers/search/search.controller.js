ea21540c72029083225673a2ca1d564570429b4d
Luke Aeschleman <lukeluke@email.unc.edu>
2016-05-02 15:26:12 -0400

    first draft of search redesign

--- blakearchive/static/controllers/search/search.controller.js
+++ blakearchive/static/controllers/search/search.controller.js
@@ -23,8 +23,10 @@
                 $scope.objectResults = results[0];
                 angular.forEach($scope.objectResults, function(works,type){
                     angular.forEach(works, function(work,index){
-                        $scope.getFirstImage(type,index);
                         //$scope.populateWorkCopies(type,index);
+                        BlakeDataService.getObject($scope.objectResults[type][index][2][0][2][0][0]).then(function (results) {
+                            $scope.objectResults[type][index][2][0][2][0][0] = results;
+                        });
                     });
                 });
                 console.log($scope.objectResults);
@@ -59,77 +61,7 @@
             maxDate: 1827
         };
 
-        vm.types = {
-            'object': {
-                'title': {
-                    'config': 'searchTitle',
-                    'text': 'Objects with a matching title',
-                    'showCopies': false,
-                    'selectedWork': '-1',
-                    'selectedCopy': '-1',
-                    'selectedObject': '-1',
-                    //'highlight': ''
-                },
-                'text': {
-                    'config': 'searchText',
-                    'text': 'Objects with matching text',
-                    'showCopies': false,
-                    'selectedWork': '-1',
-                    'selectedCopy': '-1',
-                    'selectedObject': '-1',
-                    //'highlight': 'open=text'
-                },
-                'notes': {
-                    'config': 'searchNotes',
-                    'text': 'Objects with matching editors\' notes',
-                    'showCopies': false,
-                    'selectedWork': '-1',
-                    'selectedCopy': '-1',
-                    'selectedObject': '-1',
-                    //'highlight': 'open=notes'
-                },
-                'tag': {
-                    'config': 'searchImageKeywords',
-                    'text': 'Objects with a matching image tag',
-                    'showCopies': false,
-                    'selectedWork': '-1',
-                    'selectedCopy': '-1',
-                    'selectedObject': '-1',
-                    //'highlight': 'open=desc'
-                },
-                'description': {
-                    'config': 'searchImageDescriptions',
-                    'text': 'Objects with a matching illustration description',
-                    'showCopies': false,
-                    'selectedWork': '-1',
-                    'selectedCopy': '-1',
-                    'selectedObject': '-1',
-                    //'highlight': 'open=desc'
-                }
-            },
-            'copy':{
-                'title':{
-                    'config':'searchTitle',
-                    'text': 'Copies with a matching title',
-                    //'highlight': ''
-                },
-                'copy_information':{
-                    'config':'searchCopyInformation',
-                    'text': 'Copies with matching work information or notes',
-                    //'highlight': 'tab=copy-info'
-                }
-            },
-            'work':{
-                'title':{
-                    'config':'searchTitle',
-                    'text': 'Works with a matching title'
-                },
-                'info':{
-                    'config':'searchWorkInformation',
-                    'text': 'Works with matching work information or notes'
-                }
-            }
-        };
+        
 
         /*$scope.setPagination = function(resultType,count){
             $scope.pagination[resultType] = [];
@@ -175,137 +107,7 @@
             $scope.search();
         }
 
-        $scope.populateWorkCopies = function (resultType, index) {
-            var copyBads = [],
-                copyBadMap = {},
-                objectIds = [],
-                objectIdMap = {};
-
-            if ($scope.selectedObjectSearchWork[resultType] != index) {
-                $scope.objectResults[resultType][index][2].forEach(function (copyResults,copyKey) {
-                    if (typeof copyResults[0] === "string") {
-                        copyBads.push(copyResults[0]);
-                        // We're storing a map from bad_id to its results container to simplify updating the results
-                        // with retrieved copies.
-                        copyBadMap[copyResults[0]] = copyResults;
-
-                        $scope.objectResults[resultType][index][2][copyKey][2].forEach(function (objResults) {
-                            if (typeof objResults[0] === "number") {
-                                objectIds.push(objResults[0]);
-                                // We're storing a map from bad_id to its results container to simplify updating the results
-                                // with retrieved copies.
-                                objectIdMap[objResults[0]] = objResults;
-                            }
-                        });
-                    }
-                });
-
-                if (copyBads.length > 0) {
-                    BlakeDataService.getCopies(copyBads).then(function (results) {
-                        results.forEach(function (result) {
-                            // Doing an in-place substitution of the bad_id with the relevant object
-                            copyBadMap[result.bad_id][0] = result;
-                        });
-                    });
-                }
-
-                if (objectIds.length > 0) {
-                    BlakeDataService.getObjects(objectIds).then(function (results) {
-                        results.forEach(function (result) {
-                            // Doing an in-place substitution of the bad_id with the relevant object
-                            objectIdMap[result.object_id][0] = result;
-                        });
-                    });
-                }
-
-                $scope.selectedObjectSearchWork[resultType] = index;
-            } else {
-                delete $scope.selectedObjectSearchWork[resultType];
-            }
-        };
-
-        $scope.getFirstImage = function(resultType,workIndex){
-            BlakeDataService.getObject($scope.objectResults[resultType][workIndex][2][0][2][0][0]).then(function (results) {
-                $scope.objectResults[resultType][workIndex][2][0][2][0][0] = results;
-            });
-        }
-
-        /*$scope.populateCopyObjects = function (resultType, workIndex, copyIndex) {
-            var objectIds = [];
-            var objectIdMap = {};
-
-            if ($scope.selectedObjectSearchCopy[resultType] != workIndex) {
-
-                $scope.objectResults[resultType][workIndex][2][copyIndex][2].forEach(function (objResults) {
-                    if (typeof objResults[0] === "number") {
-                        objectIds.push(objResults[0]);
-                        // We're storing a map from bad_id to its results container to simplify updating the results
-                        // with retrieved copies.
-                        objectIdMap[objResults[0]] = objResults;
-                    }
-                });
-
-                if (objectIds.length > 0) {
-                    BlakeDataService.getObjects(objectIds).then(function (results) {
-                        results.forEach(function (result) {
-                            // Doing an in-place substitution of the bad_id with the relevant object
-                            objectIdMap[result.object_id][0] = result;
-                        });
-                    });
-                }
-
-                $scope.selectedObjectSearchCopy[resultType] = workIndex;
-            } else {
-                delete $scope.selectedObjectSearchCopy[resultType];
-            }
-        };*/
-
-        vm.showCopies = function(resultType,workIndex){
-            $scope.populateWorkCopies(resultType,workIndex);
-            vm.types.object[resultType].showCopies = true;
-            vm.types.object[resultType].selectedWork = workIndex;
-            if($scope.objectResults[resultType][workIndex][1] == 1){
-                vm.types.object[resultType].selectedCopy = 0;
-                if($scope.objectResults[resultType][workIndex][2][0][1] == 1){
-                    vm.types.object[resultType].selectedObject = 0;
-                }
-            }
-        }
-        
-        vm.showObjects = function(resultType,copyIndex){
-            vm.types.object[resultType].selectedCopy = copyIndex;
-            if($scope.objectResults[resultType][vm.types.object[resultType].selectedWork][2][copyIndex][1] == 1){
-                vm.types.object[resultType].selectedObject = 0;
-            }
-        }
-        
-        vm.showHighlight = function(resultType,objectIndex){
-            vm.types.object[resultType].selectedObject = objectIndex;
-        }
-
-        vm.closeCopies = function(resultType){
-            vm.types.object[resultType].showCopies = false;
-            vm.types.object[resultType].selectedWork = -1;
-            vm.types.object[resultType].selectedCopy = -1;
-            vm.types.object[resultType].selectedObject = -1;
-        }
-        
-        $scope.$on('resize::resize',function(e,w){
-            if(w.width > 992){
-               vm.search_grid = {'width':(w.width - 80) / 3+'px'};
-               vm.copy_container = {'height':(w.height - 100)+'px'};
-               vm.copy_container_flex = {'height':(w.height - 250)+'px'};
-               vm.object_image_container = {'height':(w.height - 400)+'px'};
-            } 
-        });
-
-        $scope.selectedObjectSearchCopy = {};
-        $scope.selectedObjectSearchWork = {};
-        /*$scope.showWorks = true;
-        $scope.showCopies = true;
-        $scope.showObjects = true;*/
-
-    };
+    }
 
     controller.$inject = ['$rootScope','$scope','$location','$routeParams','BlakeDataService','$q'];
 
---
+++ blakearchive/static/directives/search-results/searchResults.directive.js
@@ -0,0 +1,127 @@
+(function(){
+
+    var controller = function($scope,BlakeDataService){
+        var vm = this;
+        
+        vm.selectedWork = -1;
+        vm.selectedCopy = -1;
+        vm.selectedObject = -1;
+        
+        vm.populateTree = function (index) {
+            var copyBads = [],
+                copyBadMap = {},
+                objectIds = [],
+                objectIdMap = {};
+
+            vm.resultTree[index][2].forEach(function (copyResults,copyKey) {
+                if (typeof copyResults[0] === "string") {
+                    copyBads.push(copyResults[0]);
+                    // We're storing a map from bad_id to its results container to simplify updating the results
+                    // with retrieved copies.
+                    copyBadMap[copyResults[0]] = copyResults;
+
+                    vm.resultTree[index][2][copyKey][2].forEach(function (objResults) {
+                        if (typeof objResults[0] === "number") {
+                            objectIds.push(objResults[0]);
+                            // We're storing a map from bad_id to its results container to simplify updating the results
+                            // with retrieved copies.
+                            objectIdMap[objResults[0]] = objResults;
+                        }
+                    });
+                }
+            });
+
+            if (copyBads.length > 0) {
+                BlakeDataService.getCopies(copyBads).then(function (results) {
+                    results.forEach(function (result) {
+                        // Doing an in-place substitution of the bad_id with the relevant object
+                        copyBadMap[result.bad_id][0] = result;
+                    });
+                });
+            }
+
+            if (objectIds.length > 0) {
+                BlakeDataService.getObjects(objectIds).then(function (results) {
+                    results.forEach(function (result) {
+                        // Doing an in-place substitution of the bad_id with the relevant object
+                        objectIdMap[result.object_id][0] = result;
+                    });
+                });
+            }
+
+        };
+
+        vm.reset = function(){
+            vm.selectedCopy = -1;
+            vm.selectedObject = -1;
+        }
+        vm.showCopies = function(workIndex){
+            vm.reset();
+            vm.populateTree(workIndex);
+            vm.selectedWork = workIndex;
+            console.log(vm.resultTree[workIndex][2].length)
+            if(vm.resultTree[workIndex][2].length == 1){
+                vm.selectedCopy = 0;
+                if(vm.resultTree[workIndex][2][0][2].length == 1){
+                    vm.selectedObject = 0;
+                }
+            }
+        }
+        
+        vm.showObjects = function(copyIndex){
+            vm.selectedCopy = copyIndex;
+            if(vm.resultTree[vm.selectedWork][2][copyIndex][2].length == 1){
+                vm.selectedObject = 0;
+            }
+        }
+        
+        vm.showHighlight = function(objectIndex){
+            vm.selectedObject = objectIndex;
+        }
+
+        vm.closeCopies = function(resultType){
+            vm.types.object[resultType].showCopies = false;
+            vm.types.object[resultType].selectedWork = -1;
+            vm.types.object[resultType].selectedCopy = -1;
+            vm.types.object[resultType].selectedObject = -1;
+        }
+        
+        $scope.$on('resize::resize',function(e,w){
+            if(w.width > 992){
+               vm.search_grid = {'width':(w.width - 80) / 3+'px'};
+               vm.copy_container = {'height':(w.height - 100)+'px'};
+               vm.copy_container_flex = {'height':(w.height - 250)+'px'};
+               vm.object_image_container = {'height':(w.height - 400)+'px'};
+            } 
+        });
+
+        $scope.selectedObjectSearchCopy = {};
+        $scope.selectedObjectSearchWork = {};
+        /*$scope.showWorks = true;
+        $scope.showCopies = true;
+        $scope.showObjects = true;*/
+
+    }
+
+    controller.$inject = ['$scope','BlakeDataService'];
+
+    var searchResults = function(){
+        return {
+            restrict: 'E',
+            templateUrl: '/blake/static/directives/search-results/searchResults.html',
+            controller: controller,
+            scope: {
+                resultTree: '=results',
+                highlight: '@highlight',
+                label: '@label',
+                type: '@type'
+            },
+            controllerAs: 'esr',
+            bindToController: true
+        };
+    }
+
+
+    angular.module('blake').directive('searchResults', searchResults);
+
+}());
