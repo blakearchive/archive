eeaa93aa32cda784a08922db926dafbabce7f628
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-05-26 13:56:17 -0400

    major changes to how BlakeDataService is used throughout

--- blakearchive/static/directives/copy-tabs/copyTabs.directive.js
+++ blakearchive/static/directives/copy-tabs/copyTabs.directive.js
@@ -3,42 +3,23 @@
  */
 (function () {
 
-    var controller = function ($scope, BlakeDataService,$routeParams,$timeout,$rootScope) {
+    /** @ngInject */
+    var controller = function ($scope,$routeParams,$timeout,$sessionStorage,BlakeDataService) {
 
         var vm = this;
+
+        vm.bds = BlakeDataService;
+
+        vm.selectedTab = '#objects-in-copy';
 
         vm.showTab = function (id) {
             vm.selectedTab = id;
         }
 
-        vm.getHeader = function(){
-            if(angular.isDefined(vm.copy)){
-                if(vm.copy.virtual == true){
-                    if(angular.isDefined(vm.work)){
-                        return vm.work.title;
-                    }
-                } else {
-                    return vm.copy.header.filedesc.titlestmt.title.main['#text'];
-                }
-            }
-        }
-        vm.getOicSubheader = function(){
-            if(angular.isDefined(vm.copy)){
-                if(vm.copy.virtual == true){
-                    return '';
-                } else {
-                    var copyPhrase = vm.copy.archive_copy_id == null ? '' : 'Copy '+vm.copy.archive_copy_id+', ';
-                    var printDate = angular.isDefined(vm.copy.source.objinfo.printdate) ? vm.copy.source.objinfo.printdate['#text'] + ' ' : '';
-                    var instPhrase = vm.copy.institutuion == null ? '' : '('+vm.copy.institution+')';
-                    return copyPhrase+printDate+instPhrase;
-                }
-            }
-        }
-
         vm.getCopyOrWork = function(){
-            if(angular.isDefined(vm.copy)){
-                if(vm.copy.virtual){
-                    if(vm.copy.bad_id == 'letters'){
+            if(angular.isDefined(vm.bds.copy)){
+                if(vm.bds.work.virtual){
+                    if(vm.bds.work.bad_id == 'letters'){
                         return 'Letter';
                     } else {
                         return 'Work';
@@ -50,9 +31,9 @@
         }
 
         vm.getCopyOrGroup = function(){
-            if(angular.isDefined(vm.copy)){
-                if(vm.copy.virtual){
-                    if(vm.copy.bad_id == 'letters'){
+            if(angular.isDefined(vm.bds.copy)){
+                if(vm.bds.work.virtual){
+                    if(vm.bds.work.bad_id == 'letters'){
                         return 'Letter';
                     } else {
                         return 'Group';
@@ -63,43 +44,45 @@
             }
         }
 
+        vm.changeObject = function(object){
+            vm.bds.changeObject(object);
+            //vm.$storage.view.mode = 'object';
+            //vm.$storage.view.scope = 'image';
+        }
 
-        $scope.$on('copyCtrl::objectChanged',function(object){
+
+        /*$scope.$on('copyCtrl::objectChanged',function(object){
             if($routeParams.descId){
                 vm.selectedTab = '#objects-in-copy';
             } else {
                 vm.selectedTab = angular.isDefined($routeParams.tab) ? '#'+$routeParams.tab : '#objects-in-copy';
             }
-        });
+        });*/
 
-        if($routeParams.tab) {
+        /*if($routeParams.tab) {
             $timeout(function () {
                 $('html, body').animate({
                     scrollTop: $('#archive-tabs').offset().top
                 }, 'slow')
             }, 0);
-        }
+        }*/
     }
 
-    controller.$inject = ['$scope', 'BlakeDataService','$routeParams','$timeout','$rootScope'];
+    var link = function(scope,ele,attr,vm){
+        var object = function(){ return vm.bds.object };
+        scope.$watch(object,function(){
+            vm.selectedTab = '#objects-in-copy';
+        },true);
+    }
 
     var copyTabs = function() {
         return {
-            restrict: 'EA',
+            restrict: 'E',
             templateUrl: "/blake/static/directives/copy-tabs/copyTabs.html",
             controller: controller,
             controllerAs: 'tabs',
-            scope: {
-                copy: '=copy',
-                work: '=work',
-                changeObject: '&',
-                resetView: '&',
-                resetCompare: '&'
-            },
-            /*link:function(scope,elem,attr){
-                scope.changeObject({object: object});
-            },*/
-            bindToController: true
+            bindToController: true,
+            link: link
         }
     }
 
