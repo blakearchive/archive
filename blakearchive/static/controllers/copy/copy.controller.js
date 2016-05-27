1627f8c9394d4a4853fcb4886ddce55cc5faf041
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-05-27 13:28:40 -0400

    completed showme

--- blakearchive/static/controllers/copy/copy.controller.js
+++ blakearchive/static/controllers/copy/copy.controller.js
@@ -5,7 +5,7 @@
 (function () {
 
     /** @ngInject */
-    var controller = function ($scope,$routeParams,$rootScope,$location,$window,$modal,$cookies,BlakeDataService,imageManipulation,CompareObjectsFactory,WindowSize) {
+    var controller = function ($scope,$routeParams,$rootScope,$window,$modal,$cookies,BlakeDataService,imageManipulation,CompareObjectsFactory) {
 
         var vm = this;
 
@@ -19,90 +19,6 @@
             $rootScope.view.mode = 'object';
             $rootScope.view.scope = 'image';
         })
-
-        /*
-         * Object and Copy selection
-         */
-        /*vm.changeCopy = function(copy_bad_id,desc_id,reset){
-            BlakeDataService.setSelectedCopy(copy_bad_id,desc_id).then(function() {
-                vm.copy = BlakeDataService.selectedCopy;
-
-                if(copy_bad_id != $routeParams.copyId){
-                    var newpath = '/blake/copy/'+copy_bad_id;
-                    $location.path(newpath, false);
-                    $routeParams.copyId = copy_bad_id;
-                    $location.search('descId',desc_id);
-                }
-
-                if(!desc_id){
-                    desc_id = vm.copy.selectedObject.desc_id;
-                }
-
-                vm.setObject(desc_id);
-
-                if(reset){
-                    vm.resetView();
-                }
-
-                var copyBad = BlakeDataService.selectedCopy.bad_id,
-                    workBadMatch = copyBad.indexOf('.'),
-                    workBad = workBadMatch > 0 ? copyBad.substring(0,workBadMatch) : copyBad;
-
-                if (angular.isUndefined(BlakeDataService.selectedWork)) {
-                    BlakeDataService.setWorkNoCopies(workBad).then(function(){
-                        vm.work = BlakeDataService.selectedWork;
-                    });
-                } else {
-                    vm.work = BlakeDataService.selectedWork;
-                }
-
-            });
-        }
-
-        vm.changeCopy($routeParams.copyId,$routeParams.descId,true);
-
-
-        vm.resetComparisonObjects = function(newObject){
-            var selectedObject = angular.isDefined(newObject) ? newObject : vm.copy.selectedObject;
-            selectedObject.isActive = true;
-            vm.$storage.comparisonObjects = [];
-            vm.$storage.comparisonObjects.push(selectedObject);
-        }
-
-        vm.setSimilarObjects = function(desc_id){
-            BlakeDataService.getObjectsFromSameMatrix(desc_id).then(function(data){
-                vm.copy.selectedObject.matrix = data;
-            });
-
-            BlakeDataService.getObjectsWithSameMotif(desc_id).then(function(data){
-                vm.copy.selectedObject.motif = data;
-            });
-
-            BlakeDataService.getObjectsFromSameProductionSequence(desc_id).then(function(data){
-                vm.copy.selectedObject.sequence = data;
-            })
-        }
-
-        vm.changeObject = function(object){
-            $location.search('descId',object.desc_id);
-        };
-
-        vm.setObject = function(desc_id){
-            angular.forEach(vm.copy.objectsInCopy,function(v){
-                if(desc_id == v.desc_id){
-                    vm.copy.selectedObject  = v;
-                    vm.setSimilarObjects(v.desc_id);
-                    imageManipulation.reset();
-                    return;
-                }
-            });
-            $rootScope.$broadcast('copyCtrl::objectChanged',vm.copy.selectedObject);
-            $rootScope.worksNavState = false;
-        }
-
-        $scope.$on('$routeUpdate',function(e,v) {
-            vm.setObject(v.params.descId);
-        });*/
 
         /*
          * Toolbar manipulation
@@ -127,22 +43,15 @@
 
             var object = {};
 
-            if(vm.$storage.view.mode == 'object'){
-                object =  vm.copy.selectedObject;
+            if($rootScope.view.mode == 'object'){
+                object =  vm.bds.object;
             }
-            if(vm.$storage.view.mode == 'compare'){
-                angular.forEach(vm.$storage.comparisonObjects, function(obj){
-                    if(obj.isActive){
-                        object = obj;
-                    }
-                });
+            if($rootScope.view.mode == 'compare'){
+                object = vm.cof.main;
             }
 
             return object;
         }
-        /*vm.newWindow = function(object){
-            $window.open('/blake/new-window/enlargement/'+object.copy_bad_id+'?descId='+object.desc_id, '_blank','width=800, height=600');
-        }*/
 
         vm.trueSizeOpen = function(object){
             if(!angular.isDefined($cookies.getObject('clientPpi'))){
@@ -157,8 +66,7 @@
         }
 
         $scope.$on('clientPpi::savedPpi',function(){
-            console.log('cppiherd');
-            $window.open('/blake/new-window/truesize/'+vm.copy.selectedObject.copy_bad_id+'?descId='+vm.copy.selectedObject.desc_id, '_blank', 'width=800, height=600');
+            $window.open('/blake/new-window/truesize/'+vm.bds.object.copy_bad_id+'?descId='+vm.bds.object.desc_id, '_blank', 'width=800, height=600');
         });
 
 
