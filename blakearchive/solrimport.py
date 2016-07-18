207f94fb65f7e842742230a4728e4036935be91a
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-07-18 15:23:55 -0400

    fixes #268

--- blakearchive/solrimport.py
+++ blakearchive/solrimport.py
@@ -23,29 +23,30 @@ def main():
     objects = session.query(models.BlakeObject).all()
     blake_object_solr.delete(q='*:*')
     for blake_object in objects:
-        obj = {
-            "id": blake_object.object_id,
-            "title": blake_object.title,
-            "bentley_id": blake_object.bentley_id,
-            "dbi": blake_object.dbi,
-            "desc_id": blake_object.desc_id,
-            "copy_id": blake_object.copy_bad_id,
-            "components": json.dumps(blake_object.components),
-            "illustration_description": json.dumps(blake_object.illustration_description),
-            "text": json.dumps(blake_object.text),
-            "copy_title": blake_object.copy.title,
-            "copy_institution": blake_object.copy.institution,
-            # FIXME: properly convert unicode rather than stripping characters
-            "notes": json.dumps([unicodedata.normalize('NFKD', note).encode('ascii', 'ignore') for note in blake_object.notes])
-        }
-        print obj["id"]
-        if blake_object.copy.work:
-            obj["work_title"] = blake_object.copy.work.title
-            obj["work_id"] = blake_object.copy.work.bad_id
-            obj["composition_date"] = blake_object.copy.composition_date
-            obj["print_date"] = blake_object.copy.print_date
-            obj["medium"] = blake_object.copy.work.medium
-        blake_object_solr.add([obj])
+        if blake_object.supplemental is False:
+            obj = {
+                "id": blake_object.object_id,
+                "title": blake_object.title,
+                "bentley_id": blake_object.bentley_id,
+                "dbi": blake_object.dbi,
+                "desc_id": blake_object.desc_id,
+                "copy_id": blake_object.copy_bad_id,
+                "components": json.dumps(blake_object.components),
+                "illustration_description": json.dumps(blake_object.illustration_description),
+                "text": json.dumps(blake_object.text),
+                "copy_title": blake_object.copy.title,
+                "copy_institution": blake_object.copy.institution,
+                # FIXME: properly convert unicode rather than stripping characters
+                "notes": json.dumps([unicodedata.normalize('NFKD', note).encode('ascii', 'ignore') for note in blake_object.notes])
+            }
+            print obj["id"]
+            if blake_object.copy.work:
+                obj["work_title"] = blake_object.copy.work.title
+                obj["work_id"] = blake_object.copy.work.bad_id
+                obj["composition_date"] = blake_object.copy.composition_date
+                obj["print_date"] = blake_object.copy.print_date
+                obj["medium"] = blake_object.copy.work.medium
+            blake_object_solr.add([obj])
     blake_object_solr.optimize()
 
     copies = session.query(models.BlakeCopy).all()
