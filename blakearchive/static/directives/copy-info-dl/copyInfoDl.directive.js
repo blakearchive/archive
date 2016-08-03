49d7e98fd6702423b800ed57f3f69f6d03707af2
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-08-03 15:05:02 -0400

    fixes #293

---
+++ blakearchive/static/directives/copy-info-dl/copyInfoDl.directive.js
@@ -0,0 +1,24 @@
+(function(){
+
+    var controller = function(){
+        var vm = this;
+    }
+
+    var copyInfoDl = function(){
+        return {
+            restrict: 'EA',
+            templateUrl: '/blake/static/directives/copy-info-dl/copyInfoDl.html',
+            controller: controller,
+            scope: {
+                infoObject: '=',
+                dt: '@',
+                highlight: '@'
+            },
+            controllerAs: 'dl',
+            bindToController: true
+        };
+    }
+
+    angular.module('blake').directive('copyInfoDl', copyInfoDl);
+
+}());
