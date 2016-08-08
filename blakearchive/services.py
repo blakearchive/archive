1eadfbe40f5ccb40f10d1fad661c26027007a06d
Nathan Rice <nathan_rice@unc.edu>
2016-08-08 11:29:06 -0400

    blake-relations.csv - fixed header field names
    copy-handprints.csv
    - fixed header field names
    
    works.csv
    - fixed header field names
    
    import.py
    - refactored
    - now
    
    services.py
    - extracted sorted BlakeObject query to a method

--- blakearchive/services.py
+++ blakearchive/services.py
@@ -1,10 +1,8 @@
-from multiprocessing.pool import worker
 import re
 import pysolr
 from sqlalchemy.sql import func
 import config
 import models
-
 
 if hasattr(config, "solr") and config.solr == "lib_prod":
     blake_object_solr = pysolr.Solr('http://webapp.lib.unc.edu:8200/solr/blake/blake-object')
@@ -15,9 +13,9 @@ elif hasattr(config, "solr") and config.solr == "lib_dev":
     blake_copy_solr = pysolr.Solr('http://webapp-dev.libint.unc.edu:8200/solr/blake/blake-copy')
     blake_work_solr = pysolr.Solr('http://webapp-dev.libint.unc.edu:8200/solr/blake/blake-work')
 elif hasattr(config, "solr") and config.solr == "local":
-     blake_object_solr = pysolr.Solr('http://localhost:8983/solr/blake_object')
-     blake_copy_solr = pysolr.Solr('http://localhost:8983/solr/blake_copy')
-     blake_work_solr = pysolr.Solr('http://localhost:8983/solr/blake_work')
+    blake_object_solr = pysolr.Solr('http://localhost:8983/solr/blake_object')
+    blake_copy_solr = pysolr.Solr('http://localhost:8983/solr/blake_copy')
+    blake_work_solr = pysolr.Solr('http://localhost:8983/solr/blake_work')
 else:
     blake_object_solr = pysolr.Solr('http://ctools-dev.its.unc.edu:8983/solr/blake-object')
     blake_copy_solr = pysolr.Solr('http://ctools-dev.its.unc.edu:8983/solr/blake-copy')
@@ -31,36 +29,37 @@ class BlakeDataService(object):
     """
 
     @classmethod
-    def generate_medium_filter(cls, config):
+    def generate_medium_filter(cls, cfg):
         mediums = []
-        if config["searchAllTypes"]:
+        if cfg["searchAllTypes"]:
             return mediums
         else:
-            if config["searchIlluminatedBooks"]:
+            if cfg["searchIlluminatedBooks"]:
                 mediums.append("illbk")
-            if config["searchCommercialBookIllustrations"]:
+            if cfg["searchCommercialBookIllustrations"]:
                 mediums.extend(["comb", "comdes", "comeng"])
-            if config["searchSeparatePrints"]:
+            if cfg["searchSeparatePrints"]:
                 mediums.extend(["spb", "spdes", "speng"])
-            if config["searchDrawingsPaintings"]:
+            if cfg["searchDrawingsPaintings"]:
                 mediums.extend(["cprint", "penc", "penink", "mono", "wc", "paint"])
-            if config["searchManuscripts"]:
+            if cfg["searchManuscripts"]:
                 mediums.extend(["ms", "ltr", "te"])
-            if config["searchRelatedMaterials"]:
+            if cfg["searchRelatedMaterials"]:
                 mediums.extend(["rmb", "rmoth"])
             return "(" + " OR ".join("medium: '%s'" % medium for medium in mediums) + ")"
 
     @classmethod
-    def generate_date_filter(cls, config):
-        min_date = config.get("minDate", "*")
-        max_date = config.get("maxDate", "*")
+    def generate_date_filter(cls, cfg):
+        min_date = cfg.get("minDate", "*")
+        max_date = cfg.get("maxDate", "*")
         date_string = ""
-        if config["useCompDate"] and config["usePrintDate"]:
-            date_string = "(composition_date: [%s TO %s] OR print_date: [%s TO %s])" % (min_date, max_date, min_date, max_date)
+        if cfg["useCompDate"] and cfg["usePrintDate"]:
+            date_ranges = (min_date, max_date, min_date, max_date)
+            date_string = "(composition_date: [%s TO %s] OR print_date: [%s TO %s])" % date_ranges
         else:
-            if config["useCompDate"]:
+            if cfg["useCompDate"]:
                 date_string = "composition_date: [%s TO %s]" % (min_date, max_date)
-            if config["usePrintDate"]:
+            if cfg["usePrintDate"]:
                 date_string = "print_date: [%s TO %s]" % (min_date, max_date)
         return date_string
 
@@ -72,14 +71,14 @@ class BlakeDataService(object):
             return "%s:'%s'" % (prefix, part_text)
 
     @classmethod
-    def generate_search_element(cls, prefix, config):
+    def generate_search_element(cls, prefix, cfg):
         regex = re.compile(r"\s+(and|or)\s+", re.IGNORECASE)
-        search_string_parts = re.split(regex, config["searchString"])
-        medium_list = cls.generate_medium_filter(config)
-        mediums = " AND "+medium_list if medium_list else ""
-        dates = cls.generate_date_filter(config)
+        search_string_parts = re.split(regex, cfg["searchString"])
+        medium_list = cls.generate_medium_filter(cfg)
+        mediums = " AND " + medium_list if medium_list else ""
+        dates = cls.generate_date_filter(cfg)
         element_parts = " ".join(cls.generate_element_part(prefix, p) for p in search_string_parts)
-        return "(" + element_parts + ") AND %s%s" % (dates,mediums)
+        return "(" + element_parts + ") AND %s%s" % (dates, mediums)
 
     @classmethod
     def solr_object_query(cls, query):
@@ -110,22 +109,22 @@ class BlakeDataService(object):
         return work_results(facets)
 
     @classmethod
-    def query_objects(cls, config):
+    def query_objects(cls, cfg):
         results = {"title": [], "tag": [], "text": [], "description": [], "notes": []}
-        if config.get("searchTitle") or config.get("searchAllFields"):
-            search_string = cls.generate_search_element("title", config)
+        if cfg.get("searchTitle") or cfg.get("searchAllFields"):
+            search_string = cls.generate_search_element("title", cfg)
             results["title"] = cls.solr_object_query(search_string)
-        if config.get("searchImageKeywords") or config.get("searchAllFields"):
-            search_string = cls.generate_search_element("components", config)
+        if cfg.get("searchImageKeywords") or cfg.get("searchAllFields"):
+            search_string = cls.generate_search_element("components", cfg)
             results["tag"] = cls.solr_object_query(search_string)
-        if config.get("searchText") or config.get("searchAllFields"):
-            search_string = cls.generate_search_element("text", config)
+        if cfg.get("searchText") or cfg.get("searchAllFields"):
+            search_string = cls.generate_search_element("text", cfg)
             results["text"] = cls.solr_object_query(search_string)
-        if config.get("searchImageDescriptions") or config.get("searchAllFields"):
-            search_string = cls.generate_search_element("illustration_description", config)
+        if cfg.get("searchImageDescriptions") or cfg.get("searchAllFields"):
+            search_string = cls.generate_search_element("illustration_description", cfg)
             results["description"] = cls.solr_object_query(search_string)
-        if config.get("searchNotes") or config.get("searchAllFields"):
-            search_string = cls.generate_search_element("notes", config)
+        if cfg.get("searchNotes") or cfg.get("searchAllFields"):
+            search_string = cls.generate_search_element("notes", cfg)
             results["notes"] = cls.solr_object_query(search_string)
 
         def add_object_query_works(results_):
@@ -135,10 +134,10 @@ class BlakeDataService(object):
         return {k: add_object_query_works(v) for (k, v) in results.items()}
 
     @classmethod
-    def query_copies(cls, config):
+    def query_copies(cls, cfg):
         results = {"copy-title": [], "copy-info": []}
-        if config.get("searchCopies") or config.get("searchAllFields"):
-            search_string = cls.generate_search_element("source", config)
+        if cfg.get("searchCopies") or cfg.get("searchAllFields"):
+            search_string = cls.generate_search_element("source", cfg)
             results["copy-info"] = cls.solr_copy_query(search_string)
 
         def add_copy_query_works(results_):
@@ -147,25 +146,30 @@ class BlakeDataService(object):
 
         return {k: add_copy_query_works(v) for (k, v) in results.items()}
 
-
     @classmethod
-    def query_works(cls, config):
+    def query_works(cls, cfg):
         results = {
             "title": {"count": 0, "results": []},
             "info": {"count": 0, "results": []}
         }
-        if config.get("searchWorks") or config.get("searchAllFields"):
-            search_string = cls.generate_search_element("info", config)
+        if cfg.get("searchWorks") or cfg.get("searchAllFields"):
+            search_string = cls.generate_search_element("info", cfg)
             solr_results = blake_work_solr.search(search_string)
             results["info"]["results"] = list(solr_results)
             results["info"]["count"] = solr_results.hits
         return results
 
+    @staticmethod
+    def get_sorted_object_query():
+        query = models.BlakeObject.query \
+            .order_by(models.BlakeObject.copy_print_date_value,
+                      models.BlakeObject.copy_composition_date_value,
+                      models.BlakeObject.object_number)
+        return query
+
     @classmethod
     def get_objects(cls, desc_ids=None):
-        query = models.BlakeObject.query \
-                .join(models.BlakeCopy) \
-                .order_by(models.BlakeObject.copy_print_date_value, models.BlakeObject.copy_composition_date_value, models.BlakeObject.object_number)
+        query = cls.get_sorted_object_query()
         if desc_ids:
             results = query.filter(models.BlakeObject.desc_id.in_(desc_ids)).all()
         else:
@@ -178,34 +182,26 @@ class BlakeDataService(object):
 
     @classmethod
     def get_objects_with_same_motif(cls, desc_id):
-        object_ = models.BlakeObject.query\
-            .filter(models.BlakeObject.desc_id == desc_id)\
-            .first()
-        return object_.objects_with_same_motif
+        obj = cls.get_sorted_object_query().filter(models.BlakeObject.desc_id == desc_id).first()
+        return obj.objects_with_same_motif
 
     @classmethod
     def get_objects_from_same_production_sequence(cls, desc_id):
-        object_ = models.BlakeObject.query\
-            .filter(models.BlakeObject.desc_id == desc_id)\
-            .first()
-        return object_.objects_from_same_production_sequence
+        obj = cls.get_sorted_object_query().filter(models.BlakeObject.desc_id == desc_id).first()
+        return obj.objects_from_same_production_sequence
 
     @classmethod
     def get_objects_from_same_matrix(cls, desc_id):
-        object_ = models.BlakeObject.query\
-            .filter(models.BlakeObject.desc_id == desc_id)\
-            .first()
-        return object_.objects_from_same_matrix
+        obj = cls.get_sorted_object_query().filter(models.BlakeObject.desc_id == desc_id).first()
+        return obj.objects_from_same_matrix
 
     @classmethod
     def get_textually_referenced_materials(cls, desc_id):
-        object_ = models.BlakeObject.query \
-            .filter(models.BlakeObject.desc_id == desc_id) \
-            .first()
+        obj = models.BlakeObject.query.filter(models.BlakeObject.desc_id == desc_id).first()
         return {
-            "objects": object_.textually_referenced_objects,
-            "copies": object_.textually_referenced_copies,
-            "works": object_.textually_referenced_works
+            "objects": obj.textually_referenced_objects,
+            "copies": obj.textually_referenced_copies,
+            "works": obj.textually_referenced_works
         }
 
     @classmethod
@@ -222,10 +218,9 @@ class BlakeDataService(object):
 
     @classmethod
     def get_objects_for_copy(cls, bad_id):
-        return models.BlakeObject.query\
-            .join(models.BlakeCopy).order_by(models.BlakeObject.object_number)\
-            .order_by(models.BlakeObject.copy_print_date_value, models.BlakeObject.copy_composition_date_value, models.BlakeObject.object_number)\
-            .filter(models.BlakeCopy.bad_id == bad_id).all()
+        query = cls.get_sorted_object_query()
+        results = query.join(models.BlakeCopy).filter(models.BlakeCopy.bad_id == bad_id).all()
+        return results
 
     @classmethod
     def get_work(cls, bad_id):
@@ -241,9 +236,9 @@ class BlakeDataService(object):
 
     @classmethod
     def get_copies_for_work(cls, bad_id):
-        return models.BlakeCopy.query\
-            .join(models.BlakeWork)\
-            .filter(models.BlakeWork.bad_id == bad_id)\
+        return models.BlakeCopy.query \
+            .join(models.BlakeWork) \
+            .filter(models.BlakeWork.bad_id == bad_id) \
             .order_by(models.BlakeCopy.print_date_value, models.BlakeCopy.composition_date_value).all()
 
     @classmethod
