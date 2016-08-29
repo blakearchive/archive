405d32008bb5f7e15623376fa2bb53557da98c5f
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-08-29 16:32:31 -0400

    prepping for publication workflow
    - removes cap files (will be kept in separate project)
    - updates readme
    - moves data out of project scope

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,16 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 
-/*gulp.task('build-css', function(){
+gulp.task('build-css', function(){
     return gulp.src(['./app/assets/bootstrap/css/bootstrap.css','./app/assets/css/*.css'])
         .pipe(cssmin())
         .pipe(concat('main.min.css'))
-        .pipe(gulp.dest('./app/assets/dist/'));
-});*/
+        .pipe(gulp.dest('./dist/'));
+});
 
 gulp.task('build-js', function(){
     return gulp.src(['blakearchive/static/js/*/*.js','blakearchive/static/js/*.js','blakearchive/static/controllers/*/*.js','blakearchive/static/directives/*/*.js'])
         .pipe(uglify())
         .pipe(concat('main.min.js'))
-        .pipe(gulp.dest('blakearchive/static/dist/'));
-});
-
-gulp.task('default',['build-js'], function(){
-
+        .pipe(gulp.dest('./dist/'));
 });
