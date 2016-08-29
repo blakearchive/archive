bae3ff367dd74044f9002b1f1c55db8dcf3eee54
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-08-29 16:32:31 -0400

    prepping for publication workflow
    - removes cap files (will be kept in separate project)
    - updates readme
    - moves data out of project scope

--- config/deploy/local.rb
+++
@@ -1,5 +0,0 @@
-set :stage, :local
-
-server fetch(:server), roles: [:local], user: fetch(:user), group: fetch(:group), no_release: true
-
-set :code_path, fetch(:cap_config)["#{fetch(:stage)}"]['local_code_dir']
--- config/deploy/qa.rb
+++
@@ -1,5 +0,0 @@
-set :stage, :qa
-
-server fetch(:server), roles: [:app], user: fetch(:user), group: fetch(:group)
-
-set :branch, qa
