bec46506c245bf1a26693acc3b2b167fbaf25103
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-09-29 15:19:50 -0400

    cleaning up for deployment
    - removed broken conf symlinks
    - random 127.0.0.1 file
    - added sold logic for dev/prod sold servers
    - removed no longer needed data dir

--- blakearchive/services.py
+++ blakearchive/services.py
@@ -5,21 +5,21 @@ import config
 import models
 
 if hasattr(config, "solr") and config.solr == "lib_prod":
-    blake_object_solr = pysolr.Solr('http://webapp.lib.unc.edu:8200/solr/blake/blake-object')
-    blake_copy_solr = pysolr.Solr('http://webapp.lib.unc.edu:8200/solr/blake/blake-copy')
-    blake_work_solr = pysolr.Solr('http://webapp.lib.unc.edu:8200/solr/blake/blake-work')
+    blake_object_solr = pysolr.Solr('http://webapp.lib.unc.edu:8200/solr/blake/blake_object')
+    blake_copy_solr = pysolr.Solr('http://webapp.lib.unc.edu:8200/solr/blake/blake_copy')
+    blake_work_solr = pysolr.Solr('http://webapp.lib.unc.edu:8200/solr/blake/blake_work')
 elif hasattr(config, "solr") and config.solr == "lib_dev":
-    blake_object_solr = pysolr.Solr('http://webapp-dev.libint.unc.edu:8200/solr/blake/blake-object')
-    blake_copy_solr = pysolr.Solr('http://webapp-dev.libint.unc.edu:8200/solr/blake/blake-copy')
-    blake_work_solr = pysolr.Solr('http://webapp-dev.libint.unc.edu:8200/solr/blake/blake-work')
+    blake_object_solr = pysolr.Solr('http://london.libint.unc.edu:8983/solr/blake_object')
+    blake_copy_solr = pysolr.Solr('http://london.libint.unc.edu:8983/solr/blake_copy')
+    blake_work_solr = pysolr.Solr('http://london.libint.unc.edu:8983/solr/blake_work')
 elif hasattr(config, "solr") and config.solr == "local":
     blake_object_solr = pysolr.Solr('http://localhost:8983/solr/blake_object')
     blake_copy_solr = pysolr.Solr('http://localhost:8983/solr/blake_copy')
     blake_work_solr = pysolr.Solr('http://localhost:8983/solr/blake_work')
 else:
-    blake_object_solr = pysolr.Solr('http://ctools-dev.its.unc.edu:8983/solr/blake-object')
-    blake_copy_solr = pysolr.Solr('http://ctools-dev.its.unc.edu:8983/solr/blake-copy')
-    blake_work_solr = pysolr.Solr('http://ctools-dev.its.unc.edu:8983/solr/blake-work')
+    blake_object_solr = pysolr.Solr('http://ctools-dev.its.unc.edu/solr/blake-object')
+    blake_copy_solr = pysolr.Solr('http://ctools-dev.its.unc.edu/solr/blake-copy')
+    blake_work_solr = pysolr.Solr('http://ctools-dev.its.unc.edu/solr/blake-work')
 
 
 class BlakeDataService(object):
