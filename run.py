6a8ba53c84a750c5ceba15a7025ddcb02317e4ad
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-11-16 11:03:03 -0500

    added path for static files

--- run.py
+++ run.py
@@ -10,13 +10,18 @@ app.config["BLAKE_DATA_SERVICE"] = services.BlakeDataService
 # The database object must initialize the app in order to be able to perform queries
 models.db.init_app(app)
 
-# @app.route('/images/<image>')
-# def get_image(image=None):
-#    if image:
-#        if config.production:
-#            return send_file("/htdocs/blake_assets/images/"+image)
-#        else:
-#            return send_file(config.local_image_path+image)
+@app.route('/images/<image>')
+def get_image(image=None):
+    if image:
+        if config.production:
+            return send_file("/htdocs/blake_assets/images/"+image)
+        else:
+            return send_file(config.local_image_path+image)
+
+@app.route('/blake/static/<file>')
+def get_file(file=None):
+    if file:
+        return send_file("static/"+file)
 
 @app.route('/', defaults={'path': ''})
 
