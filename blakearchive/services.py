2f94580aa4fa31ae76306697da68282e2e9569c6
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-11-17 14:31:26 -0500

    WIP #241

--- blakearchive/services.py
+++ blakearchive/services.py
@@ -107,7 +107,7 @@ class BlakeDataService(object):
             search_string = cls.generate_search_element("title", cfg)
             results["title"] = cls.solr_object_query(search_string)
         if cfg.get("searchImageKeywords") or cfg.get("searchAllFields"):
-            search_string = cls.generate_search_element("components", cfg)
+            search_string = cls.generate_search_element("characteristics", cfg)
             results["tag"] = cls.solr_object_query(search_string)
         if cfg.get("searchText") or cfg.get("searchAllFields"):
             search_string = cls.generate_search_element("text", cfg)
