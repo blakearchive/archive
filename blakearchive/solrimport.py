53fa6285742a0550a01befdf2656a8439817f008
Luke Aeschleman <lukeaeschleman@gmail.com>
2016-12-08 17:25:58 -0500

    solr debugging

--- blakearchive/solrimport.py
+++ blakearchive/solrimport.py
@@ -34,6 +34,7 @@ def main():
         if blake_object.supplemental is None:
             notes = []
             for note in blake_object.notes:
+                print note
                 if hasattr(note, 'note'):
                     notes.append(unicodedata.normalize('NFKD', note.note).encode('ascii', 'ignore'))
 
