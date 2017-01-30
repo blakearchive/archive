9ad5289cc2489fff0542d3b3c5bf476758902b21
Nathan Rice <nathan_rice@unc.edu>
2017-01-30 18:28:02 -0500

    Directives cleaned up

--- blakearchive/static/directives/object-reading/objectReading.directive.js
+++ blakearchive/static/directives/object-reading/objectReading.directive.js
@@ -1,72 +1,62 @@
-(function(){
+angular.module("blake").controller("ObjectReadingController", function($rootScope,BlakeDataService,$scope){
+    var vm = this;
+    vm.bds = BlakeDataService;
+    $rootScope.onWorkPage = false;
+    $scope.dpi = $rootScope.dpivalue;
+    //$rootScope.view.scope = 'both';
 
-    /** @ngInject */
-    var controller = function($rootScope,BlakeDataService,$scope){
-        var vm = this;
-        vm.bds = BlakeDataService;
-        $rootScope.onWorkPage = false;
-        $scope.dpi = $rootScope.dpivalue;
-        //$rootScope.view.scope = 'both';
-        
-        vm.getOvpTitle = function(){
-            if(angular.isDefined(vm.bds.copy)){
-                if(vm.bds.work.virtual == true){
-                    if(vm.bds.copy.bad_id == 'letters'){
-                        return vm.bds.object.object_group;
-                    } else {
-                        return vm.bds.work.title;
-                    }
+    vm.getOvpTitle = function(){
+        if(angular.isDefined(vm.bds.copy)){
+            if(vm.bds.work.virtual == true){
+                if(vm.bds.copy.bad_id == 'letters'){
+                    return vm.bds.object.object_group;
                 } else {
-                    var copyPhrase = vm.bds.copy.archive_copy_id == null ? '' : ', Copy '+vm.bds.copy.archive_copy_id;
-
-                    if(vm.bds.copy.header){
-                        copyPhrase = vm.bds.copy.header.filedesc.titlestmt.title['@reg']+copyPhrase
-                    }
-
-                    return copyPhrase;
+                    return vm.bds.work.title;
                 }
+            } else {
+                var copyPhrase = vm.bds.copy.archive_copy_id == null ? '' : ', Copy '+vm.bds.copy.archive_copy_id;
+
+                if(vm.bds.copy.header){
+                    copyPhrase = vm.bds.copy.header.filedesc.titlestmt.title['@reg']+copyPhrase
+                }
+
+                return copyPhrase;
             }
         }
-
-
-        vm.changeObject = function(object){
-            vm.bds.changeObject(object);
-            $rootScope.view.mode = 'object';
-            $rootScope.view.scope = 'image';
-            $rootScope.persistentmode = 'gallery';
-            $rootScope.states.activeItem = 'gallery';
-        }
-
-        vm.cssSafeId = function(string){
-            return string.replace(/\./g,'-');
-        }
-
-         $scope.$watch(function() {
-            return $rootScope.dpivalue;
-            }, function() {
-                 
-                if ($rootScope.dpivalue == '300') {
-                        $scope.dpi = "300";
-                }
-                else {
-                        $scope.dpi = "100";
-                }
-
-            }, true);
-        
     }
 
-
-    var objectReading = function(){
-        return {
-            restrict: 'E',
-            templateUrl: '/static/directives/object-reading/objectReading.html',
-            controller: controller,
-            controllerAs: 'read',
-            bindToController: true
-        };
+    vm.changeObject = function(object){
+        vm.bds.changeObject(object);
+        $rootScope.view.mode = 'object';
+        $rootScope.view.scope = 'image';
+        $rootScope.persistentmode = 'gallery';
+        $rootScope.states.activeItem = 'gallery';
     }
 
-    angular.module('blake').directive("objectReading", objectReading);
+    vm.cssSafeId = function(string){
+        return string.replace(/\./g,'-');
+    }
 
-}());
+     $scope.$watch(function() {
+        return $rootScope.dpivalue;
+        }, function() {
+
+            if ($rootScope.dpivalue == '300') {
+                    $scope.dpi = "300";
+            }
+            else {
+                    $scope.dpi = "100";
+            }
+
+        }, true);
+});
+
+angular.module('blake').directive("objectReading", function(){
+    return {
+        restrict: 'E',
+        template: require('html-loader!./objectReading.html'),
+        controller: "ObjectReadingController",
+        controllerAs: 'read',
+        bindToController: true
+    };
+});
--- blakearchive/static/directives/objects-from-same/objectsFromSame.directive.js
+++ blakearchive/static/directives/objects-from-same/objectsFromSame.directive.js
@@ -1,74 +1,62 @@
-/**
- * Created by nathan on 2/3/15.
- */
+angular.module("blake").controller("ObjectsFromSameController", function($rootScope,BlakeDataService,CompareObjectsFactory){
+    var vm = this;
+    vm.bds = BlakeDataService;
+    vm.cof = CompareObjectsFactory;
+    vm.compareText = "Select All Objects";
+    vm.selectedAll = false;
 
-(function(){
-
-    /** @ngInject */
-    var controller = function($rootScope,BlakeDataService,CompareObjectsFactory){
-
-        var vm = this;
-        vm.bds = BlakeDataService;
-        vm.cof = CompareObjectsFactory;
-        vm.compareText = "Select All Objects";
-        vm.selectedAll = false;
-
-        vm.selectAll = function () {
-            vm.cof.checkCompareType(vm.type);
-            if(!vm.selectedAll) {
-                vm.compareText = "Clear All Objects";
-                vm.selectedAll = true;
-                vm.cof.selectAll(vm.bds.object[vm.type]);
-            } else {
-                vm.compareText = "Select All Objects";
-                vm.selectedAll = false;
-                vm.cof.clearComparisonObjects();
-            }
-        };
-
-        // Add/remove single object for comparison
-        vm.selectOne = function(obj) {
-            vm.cof.checkCompareType(vm.type);
-            if(vm.cof.isComparisonObject(obj)) {
-                vm.cof.removeComparisonObject(obj);
-            } else {
-                vm.cof.addComparisonObject(obj);
-            }
-        };
-
-
-        vm.activateCompare = function(){
-            $rootScope.worksNavState = false;
-            $rootScope.view.mode = 'compare';
-            $rootScope.view.scope = 'image';
+    vm.selectAll = function () {
+        vm.cof.checkCompareType(vm.type);
+        if(!vm.selectedAll) {
+            vm.compareText = "Clear All Objects";
+            vm.selectedAll = true;
+            vm.cof.selectAll(vm.bds.object[vm.type]);
+        } else {
+            vm.compareText = "Select All Objects";
+            vm.selectedAll = false;
+            vm.cof.clearComparisonObjects();
         }
+    };
 
+    // Add/remove single object for comparison
+    vm.selectOne = function(obj) {
+        vm.cof.checkCompareType(vm.type);
+        if(vm.cof.isComparisonObject(obj)) {
+            vm.cof.removeComparisonObject(obj);
+        } else {
+            vm.cof.addComparisonObject(obj);
+        }
+    };
+
+
+    vm.activateCompare = function(){
+        $rootScope.worksNavState = false;
+        $rootScope.view.mode = 'compare';
+        $rootScope.view.scope = 'image';
     }
 
-    var link = function(scope,ele,attr,vm){
-        var type = function(){ return vm.cof.comparisonType };
+});
+
+angular.module('blake').directive('objectsFromSame', function () {
+    let link = function(scope,ele,attr,vm){
+        let type = function(){ return vm.cof.comparisonType };
         scope.$watch(type,function(newVal,oldVal){
             if(oldVal && newVal != vm.type){
                 vm.compareText = "Select All Objects";
                 vm.selectedAll = false;
             }
         },true);
+    };
+
+    return {
+        restrict: 'E',
+        template: require("html-loader!./objectsFromSame.html"),
+        controller: "ObjectsFromSameController",
+        controllerAs: 'fromSame',
+        scope: {
+            type: '@type'
+        },
+        bindToController: true,
+        link: link
     }
-
-    var objectsFromSame = function(){
-        return {
-            restrict: 'E',
-            templateUrl: "/static/directives/objects-from-same/objectsFromSame.html",
-            controller: controller,
-            controllerAs: 'fromSame',
-            scope: {
-                type: '@type'
-            },
-            bindToController: true,
-            link: link
-        }
-    }
-
-    angular.module('blake').directive('objectsFromSame',objectsFromSame);
-
-}());
+});
