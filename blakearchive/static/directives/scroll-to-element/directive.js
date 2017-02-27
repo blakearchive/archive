53e1b6e9341c7e450151d99685b72695a6bf60cc
Michael Fox <mrfox@live.unc.edu>
2017-02-27 11:47:41 -0500

    testing offset

--- blakearchive/static/directives/scroll-to-element/directive.js
+++ blakearchive/static/directives/scroll-to-element/directive.js
@@ -1,8 +1,9 @@
 //TODO make this a true scroll to element, rather than simple offset of current element
 angular.module("blake").directive('scrollToElement', function($timeout){
+
     var link = function(scope,element,attr) {
         element.on('click',function(){
-            let startingOffset = element.offset();
+            let startingOffset = element.offset() - 100;
             $timeout(function () {
                 let elementOffset = attr.scrollToElement ? $(attr.scrollToElement).offset() : startingOffset,
                     offset = scope.offset ? parseInt(scope.offset) : 0;
