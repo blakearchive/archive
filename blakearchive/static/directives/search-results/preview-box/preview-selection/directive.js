3318ef9bd7de722d6044c99bc074e140a96075ea
Michael Fox <foxmichaelroy@gmail.com>
2017-03-01 22:42:37 -0500

    trying again

--- blakearchive/static/directives/search-results/preview-box/preview-selection/directive.js
+++ blakearchive/static/directives/search-results/preview-box/preview-selection/directive.js
@@ -1,6 +1,10 @@
-angular.module("blake").controller("PreviewSelectionController", function (SearchService) {
+angular.module("blake").controller("PreviewSelectionController", function (SearchService, scope) {
     let vm = this;
     vm.s = SearchService;
+    
+        let vm.results = scope.results,
+            vm.s.type = scope.type,
+            vm.tree = scope.tree;
 });
 
 angular.module("blake").directive("previewSelection", function () {
@@ -8,6 +12,11 @@ angular.module("blake").directive("previewSelection", function () {
         template: require("html-loader!./template.html"),
         controller: "PreviewSelectionController",
         bindToController: true,
+        scope: {
+            results: '<results',
+            type: '<type',
+            tree: '<tree'
+        },
         controllerAs: "ps",
     }
 });
