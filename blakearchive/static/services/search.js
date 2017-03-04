5f26591edec5f1bc4a48dd1a6a61c1d7fd515d19
ba001 <ba001>
2017-03-04 10:46:27 -0500

    search done

--- blakearchive/static/services/search.js
+++ blakearchive/static/services/search.js
@@ -193,6 +193,7 @@ angular.module("blake").factory("SearchService", function ($rootScope, $location
         "yourselves"];
 
     s.search = function () {
+        s.searchdone = false;
         delete s.type;
         s.queryString = s.searchConfig.searchString;
         s.highlight = s.searchConfig.searchString;
