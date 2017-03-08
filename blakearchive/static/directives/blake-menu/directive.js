1fc173c0e180469983da4581a474098d2e0bfbe1
Tae Lerch <lerch@email.unc.edu>
2017-03-08 09:39:10 -0500

    initial proof of concept of the 'add to cart' backed by local storage.
    cart service is largely copied from some internet examples and it
    follows their implementation. It stores the cart as an array of descId
    under a specific key to a browser's localstorage. Note that we do not
    detect if the browser has the capability. I'll look into that next. If
    they have localStorage, we'll use it... If not, perhaps we fall back to
    cookies to those users.

--- blakearchive/static/directives/blake-menu/directive.js
+++ blakearchive/static/directives/blake-menu/directive.js
@@ -1,7 +1,17 @@
-angular.module("blake").controller("BlakeMenuController", function($rootScope){
+angular.module("blake").controller("BlakeMenuController", ['$scope','$rootScope','CartStorageService', function($scope,$rootScope,CartStorageService){
     let vm = this;
     vm.rs = $rootScope;
-});
+    // tie cartItems to the rootScope rather than scope?
+    // pros versus cons... TODO: analyze this! understand it!
+    // I suspect that this needs to be outside of the directive in the containing page's
+    // controller or on its own dedicated controller...
+    $rootScope.cartItems = CartStorageService.cartItems;
+    
+    // load pre-existing cart from local storage....
+    $rootScope.cartItems = CartStorageService._getFromLocalStorage();
+    
+    //console.log(CartStorageService)
+}]);
 
 angular.module("blake").directive('blakeMenu', function(){
     return {
