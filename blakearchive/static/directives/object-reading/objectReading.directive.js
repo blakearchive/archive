7d71f06bca0ac398417b5e56b8bfa055bc10e024
Michael Fox <mrfox@live.unc.edu>
2017-04-04 15:33:01 -0400

    log

--- blakearchive/static/directives/object-reading/objectReading.directive.js
+++ blakearchive/static/directives/object-reading/objectReading.directive.js
@@ -41,6 +41,7 @@ angular.module("blake").controller("ObjectReadingController", function($rootScop
                     vm.compareCopyObjects.push([]);
                 }
             });
+            console.log(compareCopyObjects);
         });
 
         /*BlakeDataService.getCopy(bad_id).then(function(result){
