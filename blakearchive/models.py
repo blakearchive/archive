095506f22878fc45e34de3d47c79f369a634d81d
Michael Fox <foxmichaelroy@gmail.com>
2017-04-09 21:15:32 -0400

    Revert "trying comp date value import for works"
    This reverts commit e66765e5385e0506a498b515a4baf893fcc0bda1.

--- blakearchive/models.py
+++ blakearchive/models.py
@@ -229,7 +229,6 @@ class BlakeWork(db.Model):
     image = db.Column(db.UnicodeText)
     composition_date = db.Column(db.Integer)
     composition_date_string = db.Column(db.UnicodeText)
-    composition_date_value = db.Column(db.UnicodeText)
     related_works = db.Column(JSON)
     virtual = db.Column(db.Boolean)
     preview = db.Column(db.Boolean)
@@ -246,7 +245,6 @@ class BlakeWork(db.Model):
             "image": self.image,
             "composition_date": self.composition_date,
             "composition_date_string": self.composition_date_string,
-            "composition_date_value": self.composition_date_value,
             "related_works": self.related_works,
             "virtual": self.virtual,
             "preview": self.preview,
