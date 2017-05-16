8b9180d3516339dbd18e54be1424d4fd9b396ef6
Michael Fox <foxmichaelroy@gmail.com>
2017-05-16 16:02:51 -0400

    detect safari

--- blakearchive/static/build/bundle.js
+++ blakearchive/static/build/bundle.js
@@ -2935,7 +2935,8 @@ angular.module('blake').controller('SearchController', ["$rootScope", "$routePar
     $rootScope.worksNavState = false;
     $rootScope.showWorkTitle = false;
 
-    var isSafari = navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0;
+    vm.s.isSafari = navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0;
+    //console.log(isSafari);
 
     $rootScope.$on("$routeChangeSuccess", function () {
         if (vm.rp.search) {
