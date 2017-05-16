b6938fc01fda202445728ae4350bb740c9f86a2b
Michael Fox <foxmichaelroy@gmail.com>
2017-05-16 15:51:55 -0400

    trying isSafari

--- blakearchive/static/build/bundle.js
+++ blakearchive/static/build/bundle.js
@@ -2935,6 +2935,10 @@ angular.module('blake').controller('SearchController', ["$rootScope", "$routePar
     $rootScope.worksNavState = false;
     $rootScope.showWorkTitle = false;
 
+    var isSafari = /constructor/i.test(window.HTMLElement) || function (p) {
+        return p.toString() === "[object SafariRemoteNotification]";
+    }(!window['safari'] || safari.pushNotification);
+
     $rootScope.$on("$routeChangeSuccess", function () {
         if (vm.rp.search) {
             vm.s.searchConfig.searchString = vm.rp.search;
--- blakearchive/static/controllers/search/search.controller.js
+++ blakearchive/static/controllers/search/search.controller.js
@@ -7,6 +7,9 @@ angular.module('blake').controller('SearchController', function($rootScope, $rou
     $rootScope.worksNavState = false;
     $rootScope.showWorkTitle = false;
 
+    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || safari.pushNotification);
+
+
     $rootScope.$on("$routeChangeSuccess", function () {
         if (vm.rp.search) {
             vm.s.searchConfig.searchString = vm.rp.search;
