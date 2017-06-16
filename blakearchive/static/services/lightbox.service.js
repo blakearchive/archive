0c3622dd3a75851d9033a95bf2d4e1c34d6f5eba
Tae Lerch <lerch@email.unc.edu>
2017-06-16 12:27:29 -0400

    bah forgot to add these objects to the commit.

---
+++ blakearchive/static/services/lightbox.service.js
@@ -0,0 +1,17 @@
+/*
+manages lightbox state: are we in crop mode or not.
+provide state transition functions for attachment to control buttons
+*/
+angular.module('blake')
+  .factory('lightbox_service',function(){
+    // note: this service is not currently being used...
+    //
+    var svc;
+    svc = {
+      'isCropMode': false,
+      startCropMode: function(){
+        this.isCropMode = true;
+      }
+    }
+    return svc;
+  });
