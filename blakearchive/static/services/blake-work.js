e6da2be70c252b29c5d79825192aa4fe78577137
ba001 <ba001>
2017-07-12 22:10:04 -0400

    plate

--- blakearchive/static/services/blake-work.js
+++ blakearchive/static/services/blake-work.js
@@ -105,6 +105,9 @@ angular.module("blake").factory("BlakeWork", function (GenericService, BlakeCopy
                             v.type = 'object';
                             v.link = v.title.link;
                             break;
+                        case 'plate':
+                            v.type = 'plate';
+                            v.link = v.title.link;
                         default:
                             v.type = "none";
                             v.link = false;
