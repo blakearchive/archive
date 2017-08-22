3a0a9a82e0b314db98a33cf32635b436d4c65a72
Michael Fox <foxmichaelroy@gmail.com>
2017-08-22 19:39:47 -0400

    ...

--- blakearchive/static/directives/object-reading/objectReading.directive.js
+++ blakearchive/static/directives/object-reading/objectReading.directive.js
@@ -123,6 +123,7 @@ angular.module("blake").controller("ObjectReadingController", function($rootScop
         vm.compareCopyObjects = {};
         vm.compareCopyObjectsTemp = [];
         vm.apparatusArray = [];
+        vm.initApparatusArray();
 
         BlakeDataService.getCopy(bad_id).then(function(resultingCopy) {
             vm.compareCopy = resultingCopy;
