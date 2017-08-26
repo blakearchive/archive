20b9aa41047d767ac71142ecd9db7445e6f6bca0
Michael Fox <foxmichaelroy@gmail.com>
2017-08-25 23:06:22 -0400

    ...

--- blakearchive/static/directives/editor-notes/editorNotes.directive.js
+++ blakearchive/static/directives/editor-notes/editorNotes.directive.js
@@ -1,6 +1,6 @@
 angular.module('blake').controller("EditorNotesController", function($routeParams) {
     var vm = this;
-    //console.log(vm.object.object_note_images);
+    console.log(vm.object.object_note_images[0].filename);
     vm.objectNotes = function () {
         if (angular.isDefined(vm.object) && angular.isDefined(vm.object.notes)) {
             return vm.object.notes.filter(function (o) {
