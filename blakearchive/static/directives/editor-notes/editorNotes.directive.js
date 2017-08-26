61a753fb1923d94300d4c971c494c74c07db3792
ba001 <ba001>
2017-08-25 21:58:17 -0400

    ...

--- blakearchive/static/directives/editor-notes/editorNotes.directive.js
+++ blakearchive/static/directives/editor-notes/editorNotes.directive.js
@@ -9,7 +9,7 @@ angular.module('blake').controller("EditorNotesController", function($routeParam
         }
     }
     vm.objectNoteImages = function () {
-        if (angular.isDefined(vm.object) && angular.isDefined(vm.object.object_note_images) && vm.object.object_note_images[0].filename != null) {
+        if (angular.isDefined(vm.object) && angular.isDefined(vm.object.object_note_images)) {
             return vm.object.object_note_images.filter(function (o) {
                     return o.type == "text"
                 }).length > 0
