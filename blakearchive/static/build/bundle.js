d56c50426d794051d8008d976b2794a5755736b6
ba001 <ba001>
2017-08-25 21:43:37 -0400

    ...

--- blakearchive/static/build/bundle.js
+++ blakearchive/static/build/bundle.js
@@ -3995,7 +3995,7 @@ angular.module("blake").directive("dpi", function () {
 
 angular.module('blake').controller("EditorNotesController", ["$routeParams", function ($routeParams) {
     var vm = this;
-    //console.log(vm.object.object_note_images);
+    console.log(vm.object.object_note_images);
     vm.objectNotes = function () {
         if (angular.isDefined(vm.object) && angular.isDefined(vm.object.notes)) {
             return vm.object.notes.filter(function (o) {
@@ -4004,7 +4004,7 @@ angular.module('blake').controller("EditorNotesController", ["$routeParams", fun
         }
     };
     vm.objectNoteImages = function () {
-        if (angular.isDefined(vm.object) && angular.isDefined(vm.object.object_note_images)) {
+        if (angular.isDefined(vm.object) && vm.object.object_note_images.length > 0) {
             return vm.object.object_note_images.filter(function (o) {
                 return o.type == "text";
             }).length > 0;
