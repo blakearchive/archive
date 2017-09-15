6a51bfffb141c9e5c3196d4856530e91751424e1
jewellthomas <jewell.thomas@maxpoint.com>
2017-09-15 16:16:25 -0400

    checkin more progress on the relationship reconciler

--- blakearchive/process_relations_csv.py
+++ blakearchive/process_relations_csv.py
@@ -3,10 +3,12 @@ import argparse
 import pandas as pd
 import difflib
 import numpy as np
-
+from tqdm import tqdm
+import pdb 
 
 logging.basicConfig()
 logger = logging.getLogger('blake_relations')
+logger.setLevel(0)
 
 id_cols = ['desc_id', 'dbi', 'bad_id', 'virtual_group']
 
@@ -31,27 +33,29 @@ def load(file_name):
 
 def flatten_to_series(df):
     """converts dataframe into series"""
-    
-    series = []
+    lst = []
     for index, row in df.iterrows():
-        series.append((index, ','.join(row.dropna().keys())))
-    pd.Series()
+        lst.append((index,'"'+','.join(row.dropna().keys()) + '"' ))
+
+    indexes = pd.DataFrame(lst)[0]
+    mappings = pd.DataFrame(lst)[1]
+    
+    return pd.Series(data=mappings, index=indexes)
 
 
 def mapped_relations_to_output_df(mapped_relations_df_dict, in_df):
 
     out_dict = {}
 
-    for key in keys:
+    for key in tqdm(keys):
         out_dict.update({key: flatten_to_series(mapped_relations_df_dict[key])})
         
-    df = pd.DataFrame(out_dict)
+    df = pd.DataFrame(out_dict) # 
+    pdb.set_trace()
     
-    df.merge(in_df, left_on='desc_id', right_on='desc_id', inplace=True)  # add id columns back on
-    
-    return df
+    return in_df[['dbi', 'bad_id', 'virtual_group']].merge(df[keys], left_index=True, right_index=True)  # add id columns back on
 
-    
+
 def normalize_relations(df):
     """
     Makes all relations in blake_relations.csv file reflexive.
@@ -61,18 +65,35 @@ def normalize_relations(df):
     result = {k:None for k in keys}
 
     for k in keys:
+        # TODO : to_lower everything, normalize names
+
         key_df = pd.DataFrame(index=df.index, columns=df.index)
 
         for desc_id, row in df[[k]].iterrows():
             if isinstance(desc_id, basestring):
                 try:
                     desc_ids = row.str.split(',')[k]
+                    error_message=''
                     if isinstance(desc_ids, list):
+                        # any relationship not inserted into the index will be dropped forever,
+                        # so we need to fail and force the user to fix the input file
+
                         try:
                             key_df.loc[[desc_id], desc_ids] = True
+                        except KeyError as e:
+                           # logger.exception(e)
+                            error_message = "error trying to associate row {} with columns {} \n".format(desc_id, desc_ids)
+
+                        try:
                             key_df.loc[desc_ids, [desc_id]] = True  # make link in both directions, a->b and b->a
                         except KeyError as e:
-                            logger.info(e)
+                            #logger.exception(e)
+                            error_message += "error trying to associate rows {} with columns {}".format(desc_ids, desc_id)
+
+                        if error_message != '':
+                            #raise Exception(error_message)
+                            import warnings
+                            #warnings.warn(error_message)
 
                 except ValueError as e:
                     assert np.isnan(desc_ids) # TODO: is this right?
@@ -92,16 +113,26 @@ def diff(in_file, out_file):
             diff = difflib.unified_diff(
                 in_file.readlines(),
                 out_file.readlines(),
-                fromfile='blake_relations.csv',
-                tofile='blake_relations_reconciled.csv',
+                fromfile=in_file,
+                tofile=out_file,
             )
+            
             for line in diff:
                 logger.info(line)
 
 
-def main(args):
 
+def main(args):
     df = load(args.file)
+
+    logger.info('blake_relations.csv size before drop duplicates: {}'.format(len(df)))
+    logger.info('counts per desc_id: {}'.format(df.groupby('desc_id').size().sort_values(ascending=False).head(15)))
+    df.drop_duplicates(subset='desc_id', keep='first', inplace=True)
+
+    logger.info('blake_relations.csv size before after duplicates: {}'.format(len(df)))
+
+    df.set_index('desc_id', inplace=True)
+
     normalize_df = normalize_relations(df)
 
     normalize_df.to_csv(args.out_file)
@@ -110,13 +141,14 @@ def main(args):
         diff(args.file, args.out_file)
 
 
-if __name__ == '__main__':
-    
-    parser = argparse.ArgumentParser()
 
-    parser.add_argument("-d", "--diff", action="store_true", default=False)
+if __name__ == 'main':
+
+    parser = argparse.ArgumentParser(description='Process a blake_relations.csv file to make all mappings reciprocal')
+
+    parser.add_argument("-d", "--diff", action="store_true", default=True)
     parser.add_argument("-o", "--out_suffix", default="reconciled")
-    parser.add_argument("-f", "--file", default="blake_relations.csv")
+    parser.add_argument("-f", "--file")
 
     _args = parser.parse_args()
 
