9359c43846ae4719a6a4e382eb85eef61d9fd508
Michael Fox <mrfox@live.unc.edu>
2017-11-02 14:58:28 -0400

    fourth attempt

--- blakearchive/models.py
+++ blakearchive/models.py
@@ -68,6 +68,7 @@ class BlakeObject(db.Model):
     copy_title = db.Column(db.UnicodeText)
     virtualwork_title = db.Column(db.UnicodeText)
     archive_copy_id = db.Column(db.Text)
+    virtualwork_id = db.Column(db.Text)
     copy_institution = db.Column(db.Text)
     copy_composition_date = db.Column(db.Integer)
     copy_composition_date_value = db.Column(db.UnicodeText)
@@ -146,6 +147,7 @@ class BlakeObject(db.Model):
             "copy_id": self.copy_id,
             "copy_title": self.copy_title,
             "virtualwork_title": self.virtualwork_title,
+            "virtualwork_id": self.virtualwork_id,
             "archive_copy_id": self.archive_copy_id,
             "copy_institution": self.copy_institution,
             "copy_composition_date": self.copy_composition_date,
