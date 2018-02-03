283ca5f679ba1bd2cc2c491976eacca8f607abbb
ba001 <ba001>
2018-02-03 11:10:57 -0500

    trying set (cont'd)

--- blakearchive/static/directives/work-title/workTitle.directive.js
+++ blakearchive/static/directives/work-title/workTitle.directive.js
@@ -50,6 +50,8 @@ angular.module("blake").controller("WorkTitleController", function ($rootScope,$
     vm.getCopyPhrase = function() {
         if(vm.bds.work.virtual){
             return '';
+        } else if(vm.bds.copy.archive_set_id != null) {
+            return  ', '+vm.bds.copy.archive_set_id;
         } else {
             return vm.bds.copy.archive_copy_id == null ? '' : 'Copy '+vm.bds.copy.archive_copy_id;
         }
