a7135d31a76ca7c0be52ba825848b24dc9309600
ba001 <ba001@users.noreply.github.com>
2018-02-14 15:04:24 -0500

    Revert "trying component concat"
    This reverts commit aec49beb83b4c52e3295f9d8426f27846da9a117.

--- blakearchive/solrimport.py
+++ blakearchive/solrimport.py
@@ -38,7 +38,7 @@ def main():
                     "copy_id": blake_object.copy_bad_id,
                     "characteristics": blake_object.characteristics,
                     "components": json.dumps(blake_object.components),
-                    "illustration_description": json.dumps(str(blake_object.illustration_description) + ' ' + ' '.join([c['#text'] for c in blake_object.components if c is not None] )),
+                    "illustration_description": json.dumps(blake_object.illustration_description),
                     "text": json.dumps(blake_object.text),
                     "copy_title": blake_object.copy.title,
                     "copy_institution": blake_object.copy.institution,
