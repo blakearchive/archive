536a00fce831b0bc582b4476abf80d170e79741a
ba001 <ba001>
2018-10-09 10:29:31 -0400

    ...

--- blakearchive/static/build/bundle.js
+++ blakearchive/static/build/bundle.js
@@ -28591,9 +28591,7 @@ angular.module("blake").controller("ObjectsFromSameController", ["$rootScope", "
 
             if (vm.type == 'textmatch') {
 
-                BlakeDataService.getFragmentPair(vm.bds.object.desc_id, obj.desc_id).then(function (resultingFragmentPair) {
-                    console.log(resultingFragmentPair);
-                });
+                console.log(BlakeDataService.getFragmentPair(vm.bds.object.desc_id, obj.desc_id));
                 console.log(vm.bds.object.desc_id);
                 //console.log("blah");
             }
