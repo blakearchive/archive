d8bf50a1c631590c819a0e420802721bf3551781
ba001 <ba001>
2018-10-30 22:32:22 -0400

    ...

--- blakearchive/static/filters/highlight.js
+++ blakearchive/static/filters/highlight.js
@@ -24,27 +24,16 @@ angular.module("blake").filter('highlight', function($sce,$rootScope){
                     if($rootScope.selectedTab == '#objects-with-text-matches') {
                         var words = ph.match(/\w+/g);
                         console.log("words:" + words);
-                         var newph = [];
-                        var i = 0;
+                        var newph = '';
                         angular.forEach(words, function (word) {
-                            if(word == 'br') {
-                                i++;
-                                return;
-                            }
-                            if(newph[i] == undefined) { newph[i] = ''; }
-                            newph[i] += word + ".*";
+                            newph += word + ".*";
                         });
+                        newph = newph.substring(0, newph.length-2);
+                        console.log("newph:" + newph);
+                        ph = newph;
                     }
                     
-                    //angular.forEach(newph, function (singleph) {
-                            //singleph = singleph.substring(0, singleph.length-2);
-                            //console.log("singleph:" + newph[1].substring(0,newph[1].length-6));
-                            //console.log(text);
-                            //console.log(singleph);
-                            text = text.replace(new RegExp('(\\b' + "newph[0]" + '[a-zA-Z]*\\b)', 'gi'), '<span class="highlighted">$1</span>');
-                            //console.log(text);
-                    //});
-
+                    text = text.replace(new RegExp('(\\b' + ph + '[a-zA-Z]*\\b)', 'gi'), '<span class="highlighted">$1</span>');
                 });
                 return text;
 
