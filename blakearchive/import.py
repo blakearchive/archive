d330b2fb4163cec41c1b624ba07a88fcdad68a00
ba001 <ba001>
2018-12-18 15:59:17 -0500

    ...

--- blakearchive/import.py
+++ blakearchive/import.py
@@ -97,7 +97,8 @@ class BlakeExhibitImporter(BlakeImporter):
       # ignored! is that OK? md is okay since it is just text.
       # if there are title sub-element(s)... use the first one
       if (len(imageXml.xpath('title'))):
-          exhibitImage.title = imageXml.xpath('title')[0].text
+        if(imageXml.xpath('title')[0].text is not None)
+          exhibitImage.title = imageXml.xpath('title')[0].text.encode("utf-8")
           #print "...has title: "+exhibitImage.title
 #      if (len(imageXml.xpath('caption'))):
 #          exhibitImage.caption = imageXml.xpath('caption')[0].text
