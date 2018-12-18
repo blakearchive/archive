0cca0807306544f4cbe596fed89db4848f8c134a
ba001 <ba001>
2018-12-18 10:01:12 -0500

    ...

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ def get_image(image=None):
     return send_file(config.local_image_path+image)
 
 @app.route('/images/exhibits/<exhibitId>/<image>')
-def get_exhibit_image(image=None):
+def get_exhibit_image(exhibitId=None,image=None):
     return send_file(config.local_image_path+'exhibits/'+exhibitId+'/'+image)
 
 @app.route('/lightbox')
