25a208c9227c3040ddc2167cc8fde19f27e2c6b2
ba001 <ba001>
2019-01-30 06:03:30 -0500

    ...

--- blakearchive/static/controllers/exhibit/exhibit.controller.js
+++ blakearchive/static/controllers/exhibit/exhibit.controller.js
@@ -29,7 +29,8 @@ angular.module('blake').controller('ExhibitController', function (
         return;
       }
       BlakeDataService.getCaptionsForImage(exhibitId, vm.images[currentIndex++].image_id).then(function(r2){
-          vm.captions.push($sce.trustAsHtml(r2));
+          r2 = $sce.trustAsHtml(r2);
+          vm.captions.push(r2);
           vm.setNextCaption();
       });
     }
