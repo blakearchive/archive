cca45897adfdff787bae3b14dc5c059fbaca35ab
ba001 <ba001>
2019-02-09 06:13:58 -0500

    Update exhibit.controller.js

--- blakearchive/static/controllers/exhibit/exhibit.controller.js
+++ blakearchive/static/controllers/exhibit/exhibit.controller.js
@@ -32,7 +32,7 @@ angular.module('blake').controller('ExhibitController', function (
           $rootScope.borderleftwidth = '0px';
           $rootScope.thumbsliderwidth = '100%';
           $rootScope.thumbslidermarginleft = '0px';
-          $rootScope.buttonsleft = '82.5%';
+          $rootScope.buttonsleft = '82.8%';
           $rootScope.galleriesMarginLeft = '0%'
         }
         else {
