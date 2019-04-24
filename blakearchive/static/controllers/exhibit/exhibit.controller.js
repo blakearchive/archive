5a3f2367f55efe01a5069cdea2b207952839f2fd
ba001 <ba001>
2019-04-24 18:30:23 -0400

    ...

--- blakearchive/static/controllers/exhibit/exhibit.controller.js
+++ blakearchive/static/controllers/exhibit/exhibit.controller.js
@@ -99,7 +99,7 @@ angular.module('blake').controller('ExhibitController', function (
         var articleContainer = document.getElementById('exhibit_article_content')
         var captionContainer = document.getElementById('reading-copy-item-0')
         var footnotesInArticle = document.querySelectorAll("div[id='exhibit_article_content'] a[class='footnote']")
-        var footnotesInCaptions = document.querySelectorAll('.captionfootnote')
+        var footnotesInCaptions = document.querySelectorAll("div.reading-copy a[class='footnote']")
         console.log(footnotesInArticle);
         console.log(footnotesInCaptions);
 
