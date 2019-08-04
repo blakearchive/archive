6cb06cec9c51ab8c7a2ad9cca8212e5a7ec4bf4c
ba001 <ba001>
2019-08-04 13:39:48 -0500

    ...

--- blakearchive/static/directives/work-title/workTitle.directive.js
+++ blakearchive/static/directives/work-title/workTitle.directive.js
@@ -26,7 +26,7 @@ angular.module("blake").controller("WorkTitleController", function ($rootScope,$
             if(vm.bds.object.object_group){
                 title = vm.bds.object.object_group;
                 title = title.match(/(\sto.*)/);
-                return title[1].charAt(1).toUpperCase() + title[1].slice(1);
+                return title[1].charAt(0).toUpperCase() + title[1].slice(1);
             }
         }
         //For Virtual Groups
