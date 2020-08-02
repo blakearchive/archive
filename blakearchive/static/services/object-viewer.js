520b929130590ee361f343cb98b1be21d4a231a0
ba001 <ba001>
2020-08-02 11:14:44 -0400

    ...

--- blakearchive/static/services/object-viewer.js
+++ blakearchive/static/services/object-viewer.js
@@ -3,8 +3,14 @@ angular.module("blake").factory("ObjectViewerService", function (BlakeDataServic
     ovs.bds = BlakeDataService;
 
     ovs.userestrictOpen = function(copy,object){
-        console.log(copy.header);
-        let header = copy.header ? copy.header.userestrict['#text'] : object.header.userestrict['#text'];
+        let header = null;
+        if(copy.header.userestrict['#text']) {
+            header = copy.header.userestrict['#text'];
+        }
+        else{
+            header = object.header.userestrict['#text'];
+        }
+        //let header = copy.header ? copy.header.userestrict['#text'] : object.header.userestrict['#text'];
         let template = 
             `<div class="modal-header">
                 <button type="button" class="close" ng-click="close()" aria-label="Close">
