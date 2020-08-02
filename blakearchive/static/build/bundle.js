2475f4727d2693b1e6fe140e74961c4774ec2171
ba001 <ba001>
2020-08-02 11:02:57 -0400

    ...

--- blakearchive/static/build/bundle.js
+++ blakearchive/static/build/bundle.js
@@ -33395,10 +33395,9 @@ angular.module("blake").factory("ObjectViewerService", ["BlakeDataService", "$mo
     ovs.bds = BlakeDataService;
 
     ovs.userestrictOpen = function (copy, object) {
+        let header = copy.header ? copy.header.userestrict['#text'] : object.header.userestrict['#text'];
         if (ovs.bds.work.virtual) {
-            let header = object.header.userestrict['#text'];
-        } else {
-            let header = copy.header ? copy.header.userestrict['#text'] : object.header.userestrict['#text'];
+            header = object.header.userestrict['#text'];
         }
         let template = `<div class="modal-header">
                 <button type="button" class="close" ng-click="close()" aria-label="Close">
