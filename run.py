7c21edf89c6720e679cdcf3ce652860b7179c37b
ba001 <ba001>
2021-01-15 16:07:48 -0500

    Update run.py

--- run.py
+++ run.py
@@ -18,9 +18,7 @@ def get_image(image=None):
 def get_exhibit_image(exhibitId=None,image=None):
     return send_file(config.local_image_path+'exhibits/'+exhibitId+'/'+image)
 
-@app.route('/images/previews/<previewId>/<image>')
-def get_preview_image(previewId=None,image=None):
-    return send_file(config.local_image_path+'previews/'+previewId+'/'+image)
+
 
 @app.route('/lightbox')
 def get_lightbox(path=None):
