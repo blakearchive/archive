2d655285c5574a489d54cc984e7c5e5d4bb7142c
ba001 <ba001>
2021-01-18 18:48:22 -0500

    ...

--- blakearchive/static/build/bundle.js
+++ blakearchive/static/build/bundle.js
@@ -26622,51 +26622,49 @@ angular.module('blake').controller('ModalController', ["$scope", "$modalInstance
 /***/ (function(module, exports) {
 
 angular.module('blake').controller('PreviewController', ["$scope", "$routeParams", "$sce", "$rootScope", "$window", "$modal", "$cookies", "BlakeDataService", "imageManipulation", "CompareObjectsFactory", "$http", function ($scope, $routeParams, $sce, $rootScope, $window, $modal, $cookies, BlakeDataService, imageManipulation, CompareObjectsFactory, $http) {
-    var vm = this;
-    var previewId = $routeParams.previewId;
-    vm.pId = previewId;
-    vm.images = [];
-    $rootScope.showWorkTitle = 'preview';
-    $rootScope.help = 'preview';
-    $rootScope.worksNavState = false;
-    //var currentIndex = 0;
-    //$rootScope.showArticle = true;
-    $rootScope.activeapparatus = 'none';
-    //$rootScope.borderleftwidth = '13px';
-    //$rootScope.thumbsliderwidth = '66%';
-    //$rootScope.thumbslidermarginleft = '13px';
-    $rootScope.buttonsleft = '74%';
-    //$rootScope.galleriesMarginLeft = '33%';
-    $rootScope.zoom = false;
+  var vm = this;
+  var previewId = $routeParams.previewId;
+  vm.pId = previewId;
+  vm.images = [];
+  $rootScope.showWorkTitle = 'preview';
+  $rootScope.help = 'preview';
+  $rootScope.worksNavState = false;
+  //var currentIndex = 0;
+  //$rootScope.showArticle = true;
+  $rootScope.activeapparatus = 'none';
+  //$rootScope.borderleftwidth = '13px';
+  //$rootScope.thumbsliderwidth = '66%';
+  //$rootScope.thumbslidermarginleft = '13px';
+  $rootScope.buttonsleft = '74%';
+  //$rootScope.galleriesMarginLeft = '33%';
+  $rootScope.zoom = false;
 
-    //vm.the_exhibit = BlakeDataService.getExhibit(exhibitId);
-    /*$scope.options = {
-            prefixUrl: "http://openseadragon.github.io/openseadragon/images/",
-            tileSources: [
-                "example-images/highsmith/highsmith.dzi"
-            ]
-        };*/
+  //vm.the_exhibit = BlakeDataService.getExhibit(exhibitId);
+  $scope.options = {
+    prefixUrl: "http://openseadragon.github.io/openseadragon/images/",
+    tileSources: ["example-images/highsmith/highsmith.dzi"]
+  };
 
-    vm.zoom = function () {
-        $rootScope.zoom = !$rootScope.zoom;
-        ////console.log($rootScope.zoom);
-    };
+  vm.zoom = function () {
+    $rootScope.zoom = !$rootScope.zoom;
+    ////console.log($rootScope.zoom);
+  };
 
-    BlakeDataService.getImagesForPreview(previewId).then(function (result) {
-        vm.images = result;
-    });
+  BlakeDataService.getImagesForPreview(previewId).then(function (result) {
+    vm.images = result;
+  });
 
-    vm.bds = BlakeDataService;
-    //console.log("Exhibit ID: "+exhibitId);
+  vm.bds = BlakeDataService;
+  //console.log("Exhibit ID: "+exhibitId);
 
 
-    $rootScope.doneSettingPreview = false;
-    vm.bds.setSelectedPreview(previewId).then(function () {
-        //console.log(">>>>>hey, tae, you were wrong!!!!");
-        $rootScope.doneSettingPreview = true;
-        console.log(vm.bds.preview);
-        //vm.scrollTo(1);
-    });
+  $rootScope.doneSettingPreview = false;
+  vm.bds.setSelectedPreview(previewId).then(function () {
+    //console.log(">>>>>hey, tae, you were wrong!!!!");
+    $rootScope.doneSettingPreview = true;
+    console.log(vm.bds.preview);
+    //vm.scrollTo(1);
+  });
 }]);
 
 /***/ }),
@@ -29197,12 +29195,7 @@ angular.module("blake").component("objectsInCopyTab", {
 /* 93 */
 /***/ (function(module, exports) {
 
-angular.module("blake").directive('openseadragon', ["$scope", function ($scope) {
-    $scope.options = {
-        prefixUrl: "http://openseadragon.github.io/openseadragon/images/",
-        tileSources: ["example-images/highsmith/highsmith.dzi"]
-    };
-}]);
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/Users/michaelfox/Sites/blake/archive/blakearchive/static/directives/openseadragon/directive.js'");
 
 /***/ }),
 /* 94 */
--- blakearchive/static/directives/openseadragon/directive.js
+++
@@ -1,8 +0,0 @@
-angular.module("blake").directive('openseadragon', function($scope){
-	$scope.options = {
-            prefixUrl: "http://openseadragon.github.io/openseadragon/images/",
-            tileSources: [
-                "example-images/highsmith/highsmith.dzi"
-            ]
-        };
-});
