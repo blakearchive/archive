533472642765f40e278f9766cb9fbd64bec10b89
ba001 <ba001>
2021-01-18 23:29:47 -0500

    Update preview.controller.js

--- blakearchive/static/controllers/preview/preview.controller.js
+++ blakearchive/static/controllers/preview/preview.controller.js
@@ -35,7 +35,7 @@ angular.module('blake').controller('PreviewController', function (
 
     BlakeDataService.getImagesForPreview(previewId).then(function(result){
       vm.images = result;
-      vm.options.tileSources.url = 'images/previews/' + vm.pId + '/' + vm.images[0]'
+      vm.options.tileSources.url = 'images/previews/' + vm.pId + '/' + vm.images[0];
     });
 
     vm.bds= BlakeDataService;
