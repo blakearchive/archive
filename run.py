c358d445772d2a1c9ef31e863f8e013d68695f63
ba001 <ba001>
2021-01-18 22:14:38 -0500

    Update run.py

--- run.py
+++ run.py
@@ -11,15 +11,15 @@ app.config["BLAKE_DATA_SERVICE"] = services.BlakeDataService
 models.db.init_app(app)
 
 @app.route('/images/<image>')
-def get_imageY(image=None):
+def get_image(image=None):
     return send_file(config.local_image_path+image)
 
 @app.route('/images/exhibits/<exhibitId>/<image>')
-def get_exhibit_imageY(exhibitId=None,image=None):
+def get_exhibit_image(exhibitId=None,image=None):
     return send_file(config.local_image_path+'exhibits/'+exhibitId+'/'+image)
 
 @app.route('/images/previews/<previewId>/<image>')
-def get_preview_imageY(previewId=None,image=None):
+def get_preview_image(previewId=None,image=None):
     return send_file(config.local_image_path+'previews/'+previewId+'/'+image)
 
 @app.route('/lightbox')
