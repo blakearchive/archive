aaebb2696051e2b1b26f97d2423194e36c7d4839
ba001 <ba001>
2021-01-22 12:21:59 -0500

    ...

--- blakearchive/static/controllers/preview/preview.controller.js
+++ blakearchive/static/controllers/preview/preview.controller.js
@@ -47,6 +47,7 @@ angular.module('blake').controller('PreviewController', function (
 */
     vm.bds= BlakeDataService;
     vm.bds.setSelectedWork($routeParams.previewId).then(function() {
+        console.log(vm.bds);
         vm.bds.setSelectedCopy(vm.bds.workCopies[0].copy_id);
         console.log(vm.bds);
         vm.options.tileSources.url = 'images/' + "BUT649.1.1r.PT.300" + '.300.jpg';
