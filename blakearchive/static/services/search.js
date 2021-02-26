ca9371c1be8bf53a88ea407c5ef49e5e9d4639f8
ba001 <ba001>
2021-02-25 23:59:20 -0500

    ...

--- blakearchive/static/services/search.js
+++ blakearchive/static/services/search.js
@@ -621,7 +621,7 @@ angular.module("blake").factory("SearchService", function (worktitleService, lig
                         return resultTree[workIndex][2][0][2][0][0].dbi + '.100.jpg';
                     }
                 case 'copy':
-                    console.log(resultTree[workIndex]);
+                    console.log(resultTree);
                     return resultTree[workIndex][2][0][0].image + '.100.jpg';
                 case 'work':
                     return resultTree[workIndex][0].image;
