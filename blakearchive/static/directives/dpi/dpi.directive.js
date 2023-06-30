52acc55af3fb8537f1c88e9cb0cafd3801794bba
ba001 <ba001>
2023-06-29 21:23:05 -0400

    ...

--- blakearchive/static/directives/dpi/dpi.directive.js
+++ blakearchive/static/directives/dpi/dpi.directive.js
@@ -2,7 +2,7 @@ angular.module('blake').controller("DpiController", function ($rootScope) {
     var vm = this;
 
     if(!angular.isDefined($rootScope.dpivalue)){
-        $rootScope.dpivalue = '100';
+        $rootScope.dpivalue = '300';
     }
 
     vm.reloadWith100or300 = function(dpiValue) {
