2968646a998ea27639f2599d86a853d3a12094f1
lfarrell <lfarrell@email.unc.edu>
2024-03-12 15:45:45 -0400

    Point solr to port 28983

--- blakearchive/services.py
+++ blakearchive/services.py
@@ -6,9 +6,9 @@ from blakearchive import models
 from blakearchive import config
 
 if hasattr(config, "solr") and config.solr == "local":
-    blake_object_solr = pysolr.Solr('http://127.0.0.1:8983/solr/blake_object')
-    blake_copy_solr = pysolr.Solr('http://127.0.0.1:8983/solr/blake_copy')
-    blake_work_solr = pysolr.Solr('http://127.0.0.1:8983/solr/blake_work')
+    blake_object_solr = pysolr.Solr('http://127.0.0.1:28983/solr/blake_object')
+    blake_copy_solr = pysolr.Solr('http://127.0.0.1:28983/solr/blake_copy')
+    blake_work_solr = pysolr.Solr('http://127.0.0.1:28983/solr/blake_work')
 else:
     blake_object_solr = pysolr.Solr('http://ctools-dev.its.unc.edu/solr/blake-object')
     blake_copy_solr = pysolr.Solr('http://ctools-dev.its.unc.edu/solr/blake-copy')
