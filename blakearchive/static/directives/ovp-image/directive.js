45378319c361471af441b97be71930948183bde9
ba001 <ba001>
2024-04-13 20:00:26 -0400

    ...

--- blakearchive/static/directives/ovp-image/directive.js
+++ blakearchive/static/directives/ovp-image/directive.js
@@ -13,18 +13,14 @@ angular.module("blake").directive('ovpImage', function(imageManipulation){
             width = image[0].naturalWidth;
             parentHeight = container.height();
             /*if(width > height && $scope.descId != 'bb128.c.te.01' && $scope.descId != 'bb128.c.te.02'){*/
-            console.log($scope.descId);
             if(width > (2*height)){  
-                console.log("hello0");
                 image.css({'height':'auto','width':'100%','margin-top':'0'});
                 me.css({'display': 'flex','justify-content': 'center', 'align-items': 'center'});
             } else if(width > height){
-                console.log("hello1");
                 var newHeight = Math.round((height * parentHeight / width));
                 var margin = Math.round(((parentHeight - newHeight) / 2));
                 image.css({'height':'auto','width':parentHeight+'px','margin-top':margin+'px'});
             } else {
-                console.log("hello2");
                 image.css({'height':'100%','width':'auto','margin-top':'0'});
             }
         });
