254422514b8d2cc7cae2bf86f5d25bc9c2b4b330
Grant Glass <Grant.glass@gmail.com>
2026-02-24 14:26:05 -0500

    Remove preview condition for Lambeth hostname
    Drop the work.preview requirement for hostname-specific logic in BlakeDocumentImporter. Previously the diff/calculation for copies and virtual_objects only ran when socket.gethostname() == 'lambeth.lib.unc.edu' and work.preview was true; now it runs on that host regardless of work.preview. Changes in blakearchive/import.py update both the copies and virtual work processing branches.

--- blakearchive/import.py
+++ blakearchive/import.py
@@ -387,7 +387,7 @@ class BlakeDocumentImporter(BlakeImporter):
         else:
             logger.error("info file does not exist: %s" % entry.info_filename)
         self.works[bad_id] = work
-        if socket.gethostname() == 'lambeth.lib.unc.edu' and work.preview:
+        if socket.gethostname() == 'lambeth.lib.unc.edu':
             diff = set(self.split_ids(entry.copies)) - set(work.preview_copies)
             result = [o for o in self.split_ids(entry.copies) if o in diff]
             all_non_preview_copies = list(result)
@@ -423,7 +423,7 @@ class BlakeDocumentImporter(BlakeImporter):
 
     def process_virtual_work(self, entry, work):
         # Virtual works need to have a special copy created just for them
-        if socket.gethostname() == 'lambeth.lib.unc.edu' and work.preview:
+        if socket.gethostname() == 'lambeth.lib.unc.edu':
             diff = set(self.split_ids(entry.virtual_objects)) - set(work.preview_copies)
             result = [o for o in self.split_ids(entry.virtual_objects) if o in diff]
             all_non_preview_objects = list(result)
